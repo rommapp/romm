4ea97e6a57563be0f74cf1f4a13047b7d68797ca
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-15 10:21:14 +0100

    dark theme by default

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -14,7 +14,7 @@ import { createVuetify } from 'vuetify'
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: 'dark',
     themes: {
       light: {
         colors: {
