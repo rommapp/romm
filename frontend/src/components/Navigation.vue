70d43e3d3b696beaa6cf3852051b4a78acdce15f
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-21 13:06:34 +0100

    ui redesign

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -14,7 +14,7 @@ const gettingRomsFlag = ref(false)
 const drawer = ref(null)
 const settings = ref(null)
 const filter = ref('')
-const rail = ref(false)
+const rail = (localStorage.getItem('rail') == 'true') ? ref(true) : ref(false)
 const theme = useTheme()
 const darkMode = (localStorage.getItem('theme') == 'light') ? ref(false) : ref(true)
 const router = useRouter()
@@ -31,6 +31,7 @@ async function getPlatforms() {
         console.log("Platforms loaded!")
         console.log(response.data.data)
         platforms.value = response.data.data
+        localStorage.setItem('platforms', JSON.stringify(response.data.data))
     }).catch((error) => {console.log(error)})
 }
 
@@ -42,6 +43,12 @@ async function selectPlatform(platform){
     emitter.emit('currentPlatform', platform.slug)
     currentPlatformName.value = platform.name
     currentPlatformSlug.value = platform.slug
+}
+
+function toggleRail(){
+    // Toggle collapsed/expand platform navigation drawer
+    rail.value = !rail.value
+    localStorage.setItem('rail', rail.value)
 }
 
 async function scan() {
@@ -71,23 +78,27 @@ getPlatforms()
 </script>
 
 <template>
+    <!-- Platforms navigation drawer -->
     <v-navigation-drawer width="250" rail-width="72" v-model="drawer" :rail="rail">
+        <v-list-item class="mt-3" fixed>
+            <template v-slot:prepend>
+                <v-avatar :rounded="0"><v-img src="/assets/romm.png"></v-img></v-avatar>
+            </template>
+            <v-list-item-title class="text-subtitle-2">Rom Manager</v-list-item-title>
+        </v-list-item>
+
+        <v-divider class="mt-3 mb-1"></v-divider>
+
         <v-list nav>
-
-            <v-list-item class="mt-1">
-                <template v-slot:prepend>
-                    <v-avatar :rounded="0"><v-img src="/assets/romm.png"></v-img></v-avatar>
-                </template>
-                <v-list-item-title class="text-subtitle-2">Rom Manager</v-list-item-title>
-            </v-list-item>
-
-            <v-divider class="mt-3 mb-1"></v-divider>
-
-            <v-list-item v-for="platform in platforms" 
-                :title="rail ? platform.slug : platform.name" 
+            <v-list-item v-for="platform in platforms"
+                class="mt-3"
+                :title="rail ? '' : platform.name" 
                 :value="platform.slug"
                 :key="platform"
                 @:click="selectPlatform(platform)">
+                <template v-slot:prepend>
+                    <v-avatar :rounded="0"><v-img :src="'/assets/platforms/'+platform.slug+'.png'"></v-img></v-avatar>
+                </template>
                 <template v-slot:append>
                     <v-progress-circular v-show="gettingRomsFlag && currentPlatformName == platform.name" indeterminate color="primary" :width="2" :size="20" class="ml-2" />
                 </template>
@@ -95,23 +106,24 @@ getPlatforms()
         </v-list>
 
         <template v-slot:append>
-            <v-btn block @click="rail = !rail" rounded="0">
+            <v-btn block @click="toggleRail()" rounded="0">
                 <v-icon v-if="rail">mdi-arrow-collapse-right</v-icon>
                 <v-icon v-if="!rail">mdi-arrow-collapse-left</v-icon>
             </v-btn>
         </template>
     </v-navigation-drawer>
 
-    <v-app-bar color="toolbar" density="compact" class="elevation-0">
-
-        <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-lg-and-up fill-height ml-0" rounded="0"/>
-
-        <v-toolbar-title class="align-center justify-center text-h6 ml-2 d-none d-sm-flex">{{ currentPlatformName }}</v-toolbar-title>
-        <v-toolbar-title class="align-center justify-center text-h6 ml-2 d-sm-none">{{ currentPlatformSlug }}</v-toolbar-title>
-
-        <v-text-field hide-details single-line label="search" prepend-inner-icon="mdi-magnify" v-model="filter" @keyup="setFilter(filter)" />
-
-        <v-app-bar-nav-icon @click="settings = !settings" class="fill-height mr-0" rounded="0"><v-icon>mdi-cog</v-icon></v-app-bar-nav-icon>
+    <v-app-bar color="toolbar" class="elevation-0" >
+        <v-app-bar-nav-icon @click="drawer = !drawer" class="hidden-lg-and-up" rounded="0"/>
+        <v-toolbar-title class="align-center text-h6 ml-4 d-none d-sm-flex">
+            <v-avatar class="mr-3" :rounded="0"><v-img :src="'/assets/platforms/'+currentPlatformSlug+'.png'"></v-img></v-avatar>
+            {{ currentPlatformName }}
+        </v-toolbar-title>
+        <v-toolbar-title class="align-center text-h6 ml-4 d-sm-none">
+            <v-avatar class="mr-3" :rounded="0"><v-img :src="'/assets/platforms/'+currentPlatformSlug+'.png'"></v-img></v-avatar>
+        </v-toolbar-title>
+        <v-text-field hide-details label="search" variant="outlined" density="compact" clearable prepend-inner-icon="mdi-magnify" v-model="filter" @keyup="setFilter(filter)" @click:clear="setFilter('')"/>
+        <v-app-bar-nav-icon @click="settings = !settings" class="mr-2 ml-2" rounded="0"><v-icon>mdi-cog</v-icon></v-app-bar-nav-icon>
     </v-app-bar>
 
     <v-navigation-drawer width="190" v-model="settings" location="right" temporary>
@@ -131,3 +143,9 @@ getPlatforms()
     </v-navigation-drawer>
 
 </template>
+
+<style scoped>
+.v-navigation-drawer--rail:not(.v-navigation-drawer--is-hovering) .v-list .v-avatar {
+  --v-avatar-height: 40px;
+}
+</style>
