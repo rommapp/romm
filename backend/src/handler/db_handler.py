b29d8c12d40772ca5c9f1bfa6394320859ae1a2d
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-24 11:19:01 +0100

    code cleaning

--- backend/src/handler/db_handler.py
+++ backend/src/handler/db_handler.py
@@ -1,7 +1,8 @@
 from fastapi import HTTPException
-
 from sqlalchemy import select
-from models.base import Session, engine, BaseModel
+from sqlalchemy.exc import ProgrammingError
+
+from models.base import Session, BaseModel, engine
 from models.platform import Platform
 from models.rom import Rom
 from logger.logger import log
@@ -17,25 +18,41 @@ class DBHandler:
         with Session.begin() as session:
             session.merge(Platform(**kargs))
 
+
+    def add_platform(self, **kargs) -> None:
+        with Session.begin() as session:
+            session.merge(Platform(**kargs))
+
     
     def get_platforms(self) -> list[Platform]:
-        with Session.begin() as session:
-            return session.scalars(select(Platform)).all()
+        try:
+            with Session.begin() as session:
+                return session.scalars(select(Platform)).all()
+        except ProgrammingError as e:
+            raise HTTPException(status_code=404, detail=f"Platforms table not found: {e}")
+        
 
-
-    def add_rom(self, **kargs) -> None:
-        with Session.begin() as session:
-            session.merge(Rom(**kargs))
+    def add_platform(self, **kargs) -> None:
+        try:
+            with Session.begin() as session:
+                session.merge(Platform(**kargs))
+        except ProgrammingError as e:
+            raise HTTPException(status_code=404, detail=f"Roms table not found: {e}")
 
 
     def get_roms(self, p_slug: str) -> list[Rom]:
         with Session.begin() as session:
             return session.scalars(select(Rom).filter_by(p_slug=p_slug)).all()
-        
-    
+
+
     def get_rom(self, p_slug: str, filename: str) -> Rom:
         with Session.begin() as session:
             return session.scalars(select(Rom).filter_by(p_slug=p_slug, filename=filename)).first()
+
+
+    def add_rom(self, **kargs) -> None:
+        with Session.begin() as session:
+            session.merge(Rom(**kargs))
     
 
     def update_rom(self, p_slug: str, filename: str, data: dict) -> None:
