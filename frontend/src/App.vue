59b71c671335f8bb40acdfa46ea30a9be798460f
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-24 12:49:17 +0100

    snackbars added to change and scan

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,10 +6,18 @@ import Navigation from '@/components/Navigation.vue'
 // Props
 useTheme().global.name.value = localStorage.getItem('theme') || 'dark'
 const refresh = ref(false)
+const refreshRoms = ref(false)
+const snackbarShow = ref(false)
+const snackbarStatus = ref({})
 
 // Event listeners bus
 const emitter = inject('emitter')
-emitter.on('scanning', () => { refresh.value = !refresh.value })
+emitter.on('refresh', () => { refresh.value = !refresh.value })
+emitter.on('refreshRoms', () => { refreshRoms.value = !refreshRoms.value })
+emitter.on('snackbarScan', (snackbar) => {
+  snackbarShow.value = true
+  snackbarStatus.value = snackbar
+})
 </script>
 
 <template>
@@ -19,9 +27,17 @@ emitter.on('scanning', () => { refresh.value = !refresh.value })
     
     <v-main>
       <v-container fluid>
-        <router-view :key="refresh"/>
+        <router-view :key="refreshRoms"/>
       </v-container>
     </v-main>
+
+    <v-snackbar v-model="snackbarShow" :timeout="3000">
+        <v-icon :icon="snackbarStatus.icon" :color="snackbarStatus.color" class="ml-2 mr-2"/>
+        {{ snackbarStatus.msg }}
+        <template v-slot:actions>
+            <v-btn variant="text" @click="snackbarShow = false"><v-icon icon="mdi-close"/></v-btn>
+        </template>
+    </v-snackbar>
 
   </v-app>
 </template>
