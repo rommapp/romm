436e7a7b235a2c8d98f1a6371786dbabf92adf5f
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-25 01:09:31 +0100

    added transition to rom title in gallery

--- frontend/src/components/RomsGallery.vue
+++ frontend/src/components/RomsGallery.vue
@@ -6,6 +6,7 @@ import { downloadRom, downloadSave } from '@/utils/utils.js'
 
 // Props
 const roms = ref([])
+const gettingRoms = ref(true)
 const noRoms = ref(false)
 const romsFiltered = ref([])
 const currentFilter = ref('')
@@ -23,6 +24,7 @@ async function getRoms(platform) {
     noRoms.value = false
     console.log("Getting roms...")
     emitter.emit('gettingRoms', true)
+    gettingRoms.value = true
     await axios.get('/api/platforms/'+platform+'/roms').then((response) => {
         console.log("Roms loaded!")
         console.log(response.data.data)
@@ -30,6 +32,7 @@ async function getRoms(platform) {
         setFilter(currentFilter.value)
     }).catch((error) => {console.log(error)})
     emitter.emit('gettingRoms', false)
+    gettingRoms.value = false
     if(roms.value.length==0){noRoms.value = true}
 }
 
@@ -60,29 +63,39 @@ onMounted(() => { if(localStorage.getItem('currentPlatform')){ getRoms(JSON.pars
         <v-col v-for="rom in romsFiltered" cols="6" xs="6" sm="3" md="3" lg="2">
             <v-hover v-slot="{isHovering, props}">
                 <v-card v-bind="props" :class="{'on-hover': isHovering}" :elevation="isHovering ? 20 : 3">
-                    <v-img :src="rom.path_cover_l+'?reload='+forceImgReload" :lazy-src="rom.path_cover_s+'?reload='+forceImgReload" cover>
-                        <v-tooltip activator="parent" open-delay="500" location="top">{{ rom.filename }}</v-tooltip>
-                        <template v-slot:placeholder>
-                            <div class="d-flex align-center justify-center fill-height">
-                                <v-progress-circular indeterminate/>
-                            </div>
-                        </template>
-                        <div v-if="!rom.has_cover" class="rom-title d-flex align-center text-body-1 pt-2 pr-5 pb-2 pl-5 bg-secondary">{{ rom.filename }}</div>
-                        <v-btn @click="selectRom(rom)" class="d-flex align-center justify-center fill-height" color="transparent" block/>
-                    </v-img>
-                    <v-card-text>
-                        <v-row>
-                            <v-btn @click="downloadRom(rom)" icon="mdi-download" size="small" variant="text"/>
-                            <v-btn @click="downloadSave()" icon="mdi-content-save-all" size="small" variant="text" :disabled="!saveFiles"/>
-                        </v-row>
-                    </v-card-text>
+                    <v-hover v-slot="{ isHovering, props }" open-delay="1000">
+                        <v-img v-bind="props" :src="rom.path_cover_l+'?reload='+forceImgReload" :lazy-src="rom.path_cover_s+'?reload='+forceImgReload" cover>
+                            <template v-slot:placeholder>
+                                <div class="d-flex align-center justify-center fill-height">
+                                    <v-progress-circular indeterminate/>
+                                </div>
+                            </template>
+                            <v-expand-transition>
+                                <div v-if="isHovering || !rom.has_cover" class="rom-title d-flex transition-fast-in-fast-out bg-secondary text-caption-1">
+                                <v-list-item>{{ rom.filename }}</v-list-item>
+                                </div>
+                            </v-expand-transition>
+                            <v-btn @click="selectRom(rom)" class="d-flex align-center justify-center fill-height" color="transparent" block/>
+                        </v-img>
+                        <v-card-text>
+                            <v-row>
+                                <v-btn @click="downloadRom(rom)" icon="mdi-download" size="small" variant="text"/>
+                                <v-btn @click="downloadSave()" icon="mdi-content-save-all" size="small" variant="text" :disabled="!saveFiles"/>
+                            </v-row>
+                        </v-card-text>
+                    </v-hover>
                 </v-card>
             </v-hover>
         </v-col>
     </v-row>
-    <v-row v-if="noRoms" class="d-flex align-center justify-center fill-height">
-        <div class="text-h6 mt-16 pt-16">Feels cold here... <v-icon>mdi-emoticon-sad</v-icon></div>
-    </v-row>
+
+    <v-dialog v-model="gettingRoms" scroll-strategy="none" width="auto" :scrim="false">
+        <v-progress-circular :width="3" :size="70" indeterminate/>
+    </v-dialog>
+
+    <v-dialog v-model="noRoms" scroll-strategy="none" width="auto" :scrim="false">
+        <div class="text-h6">Feels cold here... <v-icon>mdi-emoticon-sad</v-icon></div>
+    </v-dialog>
 
 </template>
 
