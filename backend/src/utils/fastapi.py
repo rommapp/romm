e3c4ba594b9d7fa5aae5a1e7946f0b42159bad88
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-27 01:40:36 +0200

    n_roms added to platform model

--- backend/src/utils/fastapi.py
+++ backend/src/utils/fastapi.py
@@ -29,7 +29,8 @@ def scan_platform(overwrite: bool, p_slug: str, igdbh: IGDBHandler, dbh: DBHandl
     if (overwrite or not fs.p_logo_exists(p_slug)) and url_logo:
         fs.store_p_logo(p_slug, url_logo)
     if fs.p_logo_exists(p_slug):
-        platform['path_logo']: str = fs.get_p_path_logo(p_slug)
+        platform['path_logo'] = fs.get_p_path_logo(p_slug)
+    platform['n_roms'] = len(fs.get_roms(p_slug))
     dbh.add_platform(**platform)
     return p_igdb_id
 
@@ -38,7 +39,7 @@ def scan_rom(overwrite: bool, rom, p_igdb_id: str, p_slug: str, igdbh: IGDBHandl
     log.info(f"Getting {rom['filename']} details")
     r_igdb_id, filename_no_ext, r_slug, r_name, summary, url_cover = igdbh.get_rom_details(rom['filename'], p_igdb_id, r_igbd_id)
     path_cover_s, path_cover_l, has_cover = fs.get_cover_details(overwrite, p_slug, filename_no_ext, url_cover)
-    {
+    rom: dict = {
         'filename': rom['filename'], 'filename_no_ext': filename_no_ext, 'size': rom['size'],
         'r_igdb_id': r_igdb_id, 'p_igdb_id': p_igdb_id,
         'name': r_name, 'r_slug': r_slug, 'p_slug': p_slug,
