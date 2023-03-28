707c4b81e503bf40b93c25d93e80a9e6dea2612b
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-28 17:03:28 +0200

    download feedback added as notification

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -6,7 +6,7 @@ export function downloadRom(rom) {
     console.log("Downloading "+rom.filename)
     axios.get('/assets/library/'+rom.p_slug+'/roms/'+rom.filename, { responseType: 'blob' }).then(response => {
         saveAs(new Blob([response.data], { type: 'application/file' }), rom.filename)
-    }).catch(console.error)
+    }).catch((error) => {console.log(error)})
 }
 
 export function downloadSave(rom) {
