24a6f8e57d6022a71135b595461328280d4e8b57
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-29 16:47:02 +0200

    allow any branch to build dev docker image

--- .github/actions/build.sh
+++ .github/actions/build.sh
@@ -1,6 +1,6 @@
-if [[ $GIT_BRANCH = 'develop' ]]; then
+if [[ $GIT_BRANCH != 'master' ]]; then
     docker buildx build --push\
-        --tag zurdi15/romm:dev-${VERSION}\
+        --tag zurdi15/romm:dev-latest --tag zurdi15/romm:dev-${VERSION}\
         --platform linux/arm64 . --file ./docker/Dockerfile
 else
     docker buildx build --push\
