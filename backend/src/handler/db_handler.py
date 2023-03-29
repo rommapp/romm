a09ffe8d69c2a3f798b135e67e425a6fd0c26961
zurdi zurdo <zurdizurdo25@gmail.com>
2023-03-29 13:00:01 +0200

    some codebase refactor

--- backend/src/handler/db_handler.py
+++ backend/src/handler/db_handler.py
@@ -23,60 +23,19 @@ class DBHandler:
         return wrapper
 
     
-    def add_platform(self, **kargs) -> None:
-        with Session.begin() as session:
-            session.merge(Platform(**kargs))
+    def add_platform(self, Platform: Platform) -> None:
+        try:
+            with Session.begin() as session:
+                session.merge(Platform)
+        except ProgrammingError as e:
+            raise HTTPException(status_code=404, detail=f"Platforms table not found: {e}")
 
-
-    def add_platform(self, **kargs) -> None:
-        with Session.begin() as session:
-            session.merge(Platform(**kargs))
-
-    
     def get_platforms(self) -> list[Platform]:
         try:
             with Session.begin() as session:
                 return session.scalars(select(Platform).order_by(Platform.slug.asc())).all()
         except ProgrammingError as e:
             raise HTTPException(status_code=404, detail=f"Platforms table not found: {e}")
-        
-
-    def add_platform(self, **kargs) -> None:
-        try:
-            with Session.begin() as session:
-                session.merge(Platform(**kargs))
-        except ProgrammingError as e:
-            raise HTTPException(status_code=404, detail=f"Roms table not found: {e}")
-
-
-    def get_roms(self, p_slug: str) -> list[Rom]:
-        with Session.begin() as session:
-            return session.scalars(select(Rom).filter_by(p_slug=p_slug).order_by(Rom.filename.asc())).all()
-
-
-    def get_rom(self, p_slug: str, filename: str) -> Rom:
-        with Session.begin() as session:
-            return session.scalars(select(Rom).filter_by(p_slug=p_slug, filename=filename)).first()
-
-
-    def add_rom(self, **kargs) -> None:
-        with Session.begin() as session:
-            session.merge(Rom(**kargs))
-    
-
-    def update_rom(self, p_slug: str, filename: str, data: dict) -> None:
-        with Session.begin() as session:
-            session.query(Rom) \
-                .filter(Rom.p_slug==p_slug, Rom.filename==filename) \
-                .update(data, synchronize_session='evaluate')
-
-
-    def delete_rom(self, p_slug: str, filename: str) -> None:
-        with Session.begin() as session:
-            session.query(Rom) \
-                .filter(Rom.p_slug==p_slug, Rom.filename==filename) \
-                .delete(synchronize_session='evaluate')
-
 
     def purge_platforms(self, platforms: list) -> None:
         with Session.begin() as session:
@@ -84,6 +43,29 @@ class DBHandler:
                 .filter(Platform.slug.not_in(platforms)) \
                 .delete(synchronize_session='evaluate')
 
+    def add_rom(self, **kargs) -> None:
+        with Session.begin() as session:
+            session.merge(Rom(**kargs))
+
+    def get_roms(self, p_slug: str) -> list[Rom]:
+        with Session.begin() as session:
+            return session.scalars(select(Rom).filter_by(p_slug=p_slug).order_by(Rom.filename.asc())).all()
+
+    def get_rom(self, p_slug: str, filename: str) -> Rom:
+        with Session.begin() as session:
+            return session.scalars(select(Rom).filter_by(p_slug=p_slug, filename=filename)).first()
+
+    def update_rom(self, p_slug: str, filename: str, data: dict) -> None:
+        with Session.begin() as session:
+            session.query(Rom) \
+                .filter(Rom.p_slug==p_slug, Rom.filename==filename) \
+                .update(data, synchronize_session='evaluate')
+
+    def delete_rom(self, p_slug: str, filename: str) -> None:
+        with Session.begin() as session:
+            session.query(Rom) \
+                .filter(Rom.p_slug==p_slug, Rom.filename==filename) \
+                .delete(synchronize_session='evaluate')
 
     def purge_roms(self, p_slug: str, roms: list) -> None:
         with Session.begin() as session:
