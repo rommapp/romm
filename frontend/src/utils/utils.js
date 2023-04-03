1b7a419120a56b1b1a342f8d0a0cfbfaf7de32b0
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-03 16:40:07 +0200

    snackbar added to gathering and download rom

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -2,13 +2,21 @@ import axios from 'axios'
 import { saveAs } from 'file-saver'
 
 
-export function downloadRom(rom) {
+export function downloadRom(rom, emitter) {
     console.log("Downloading "+rom.file_name)
+    emitter.emit('snackbarScan', {'msg': "Gathering "+rom.file_name+"...", 'icon': 'mdi-download', 'color': 'green'})
+    emitter.emit('downloadingRom', true)
     axios.get('/assets'+rom.file_path+'/'+rom.file_name, { responseType: 'blob' }).then(response => {
+        emitter.emit('downloadingRom', false, rom.file_name)
+        emitter.emit('snackbarScan', {'msg': "Downloading "+rom.file_name, 'icon': 'mdi-download', 'color': 'green'})
         saveAs(new Blob([response.data], { type: 'application/file' }), rom.file_name)
-    }).catch((error) => {console.log(error)})
+    }).catch((error) => {
+        emitter.emit('downloadingRom', false)
+        console.log(error)
+        emitter.emit('snackbarScan', {'msg': "Couldn't download "+rom.file_name+". Something went wrong...", 'icon': 'mdi-close-circle', 'color': 'red'})
+    })
 }
 
-export function downloadSave(rom) {
+export function downloadSave(rom, emitter) {
     console.log("Downloading "+rom.file_name+" save file")
 }
