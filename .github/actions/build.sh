b12e88390f8cc22a495201bc8efb67a0a536294f
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-04 17:03:10 +0200

    refactor versioning ci/cd

--- .github/actions/build.sh
+++ .github/actions/build.sh
@@ -1,9 +1,13 @@
+VERSION=$(cat ./frontend/.env | grep VITE_ROMM_VERSION | cut -d "=" -f2)
+
 if [[ $GIT_BRANCH != 'master' ]]; then
-    docker buildx build --push\
-        --tag zurdi15/romm:dev-latest --tag zurdi15/romm:dev-${VERSION}\
-        --platform linux/arm64 . --file ./docker/Dockerfile
-else
-    docker buildx build --push\
-        --tag zurdi15/romm:latest --tag zurdi15/romm:${VERSION}\
-        --platform linux/arm64,linux/amd64 . --file ./docker/Dockerfile
+    VERSION=dev-$VERSION
+    echo $VERSION
+    # docker buildx build --push\
+    #     --tag zurdi15/romm:dev-latest --tag zurdi15/romm:dev-${VERSION}\
+    #     --platform linux/arm64 . --file ./docker/Dockerfile
+# else
+#     docker buildx build --push\
+#         --tag zurdi15/romm:latest --tag zurdi15/romm:${VERSION}\
+#         --platform linux/arm64,linux/amd64 . --file ./docker/Dockerfile
 fi
