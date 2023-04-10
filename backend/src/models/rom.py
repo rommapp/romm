14edfe243f7a078a67d8ab841854bbc41afd183a
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-10 16:31:17 +0200

    multifile selector added to front

--- backend/src/models/rom.py
+++ backend/src/models/rom.py
@@ -17,7 +17,7 @@ class Rom(BaseModel):
     file_name_no_tags = Column(String(length=450), default="")
     file_extension = Column(String(length=10), default="")
     file_path = Column(String(length=1000), default="")
-    file_size = Column(Float, default="")
+    file_size = Column(Float, default=0.0)
     
     name = Column(String(length=350), default="")
     r_slug = Column(String(length=100), default="")
@@ -33,6 +33,7 @@ class Rom(BaseModel):
     tags = Column(JSON, default=[])
 
     multi = Column(Boolean, default=False)
+    files = Column(JSON, default=[])
 
     @property
     def full_path(self) -> str:
@@ -47,4 +48,5 @@ class Rom(BaseModel):
             f"file_name: {self.file_name},"
             f"file_path: {self.file_path},"
             f"multi: {self.multi},"
+            f"files: {self.files}"
         )
