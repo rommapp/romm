07f72f10517a3d9721a9f5fea936e370a1ea8596
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-11 16:25:41 +0200

    clearer logs

--- backend/src/utils/fastapi.py
+++ backend/src/utils/fastapi.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from handler import igdbh, dbh
+from handler import igdbh
 from utils import fs
 from models.platform import Platform
 from models.rom import Rom
@@ -27,16 +27,13 @@ def scan_platform(p_slug: str) -> Platform:
     Returns
         Platform object
     """
-    log.info(f"Getting {p_slug} details")
     platform_attrs: dict = igdbh.get_platform_details(p_slug)
     platform_attrs['n_roms'] = fs.get_roms(p_slug, True, only_amount=True)
-    log.info(f"Roms found for {platform_attrs['name']}: {platform_attrs['n_roms']}")
     platform = Platform(**platform_attrs)
     return platform
 
 
 def scan_rom(platform: Platform, rom: dict, r_igbd_id_search: str = '', overwrite: bool = False) -> None:
-    log.info(f"Getting {rom['file_name']} details")
     r_igdb_id, file_name_no_tags, r_slug, r_name, summary, url_cover = igdbh.get_rom_details(rom['file_name'], platform.igdb_id, r_igbd_id_search)
     path_cover_s, path_cover_l, has_cover = fs.get_cover_details(overwrite, platform.slug, rom['file_name'], url_cover)
     rom['file_name_no_tags'] = file_name_no_tags
@@ -50,4 +47,4 @@ def scan_rom(platform: Platform, rom: dict, r_igbd_id_search: str = '', overwrit
     rom['path_cover_l'] = path_cover_l
     rom['has_cover'] = has_cover
     rom = Rom(**rom)
-    dbh.add_rom(rom)
+    return rom
