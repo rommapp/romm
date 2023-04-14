b135b893c783311af8e05393bbedcb93fc8f5f1c
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-14 15:02:46 +0200

    supress warning message when bad config file

--- frontend/src/components/AppBar/Base.vue
+++ frontend/src/components/AppBar/Base.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, inject } from "vue"
+import { useRouter } from 'vue-router'
 import PlatformsBtn from '@/components/AppBar/PlatformsBtn.vue'
 import UploadBtn from '@/components/AppBar/UploadBtn.vue'
 import SearchBar from '@/components/AppBar/SearchBar.vue'
@@ -8,22 +9,25 @@ import SettingsBtn from '@/components/AppBar/SettingsBtn.vue'
 
 // Props
 const scanning = ref(false)
+const router = useRouter()
 
 // Event listeners bus
 const emitter = inject('emitter')
 emitter.on('scanning', (s) => { scanning.value = s })
+
+// Functions
+async function goHome(){
+    await router.push(import.meta.env.BASE_URL)
+}
 </script>
 
 <template>
 
     <v-app-bar class="elevation-3">
 
-        <v-progress-linear
-            :active="scanning"
-            :indeterminate="true"
-            absolute/>
+        <v-progress-linear :active="scanning" :indeterminate="true" absolute/>
 
-        <v-avatar size="100" class="ml-3"><v-img src="/assets/romm_home.svg"></v-img></v-avatar>
+        <v-avatar size="100" class="ml-3 home-btn hidden-sm-and-down" @click="goHome()"><v-img src="/assets/romm.svg"></v-img></v-avatar>
             
         <platforms-btn/>
         
@@ -41,3 +45,9 @@ emitter.on('scanning', (s) => { scanning.value = s })
     </v-app-bar>
 
 </template>
+
+<style scoped>
+.home-btn{
+    cursor: pointer;
+}
+</style>
