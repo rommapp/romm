2ba931e6e6e7f3a191a368ef0504df4d6b2c12fd
Justin Forlenza <justin@forlenza.co>
2023-04-16 18:37:06 -0400

    Removed references to local storage

--- frontend/src/components/PlatformsDrawer/Base.vue
+++ frontend/src/components/PlatformsDrawer/Base.vue
@@ -6,7 +6,6 @@ import RailBtn from '@/components/PlatformsDrawer/RailBtn.vue'
 import Platform from '@/components/PlatformsDrawer/Platform.vue'
 
 // Props
-const selectedPlatform = ref(JSON.parse(localStorage.getItem('selectedPlatform')) || "")
 const platforms = ref([])
 const platformsDrawer = ref(null)
 const rail = (localStorage.getItem('rail') == 'true') ? ref(true) : ref(false)
@@ -30,7 +29,6 @@ async function goHome(){
 }
 
 getPlatforms()
-emitter.emit('selectedPlatform', selectedPlatform.value)
 </script>
 
 <template>
