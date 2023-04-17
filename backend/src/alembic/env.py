1fee0e8cb73ca0e1d9a8b2a12659ba298d276374
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-17 18:07:28 +0200

    changed alembic migrations to batch mode

--- backend/src/alembic/env.py
+++ backend/src/alembic/env.py
@@ -47,6 +47,7 @@ def run_migrations_offline() -> None:
     context.configure(
         url=get_db_engine(),
         target_metadata=target_metadata,
+        render_as_batch=True,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
     )
@@ -67,7 +68,9 @@ def run_migrations_online() -> None:
 
     with engine.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            render_as_batch=True
         )
 
         with context.begin_transaction():
