ad0abb49174703a402d5da36bd4cbb613cd09aff
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-17 10:27:26 +0200

    router link added to gallery cards

--- frontend/src/components/GameGallery/Card/Cover.vue
+++ frontend/src/components/GameGallery/Card/Cover.vue
@@ -1,53 +1,51 @@
 <script setup>
-import { inject } from 'vue'
-import { useRouter } from 'vue-router'
-
 // Props
 const props = defineProps(['rom', 'isHovering', 'hoverProps', 'size'])
 const forceImgReload = Date.now()
-const router = useRouter()
-
-// Event listeners bus
-const emitter = inject('emitter')
 </script>
 
 <template>
-    <v-img 
-        :to="`/${$route.params.platform}/`"
-        v-bind="hoverProps"
-        :src="'/assets'+rom.path_cover_l+'?reload='+forceImgReload"
-        :lazy-src="'/assets'+rom.path_cover_s+'?reload='+forceImgReload"
-        class="cover"
-        cover>
-        <template v-slot:placeholder>
-            <div class="d-flex align-center justify-center fill-height">
-                <v-progress-circular color="secondary" indeterminate/>
-            </div>
-        </template>
-        <v-expand-transition>
-            <div 
-                v-if="isHovering || !rom.has_cover"
-                class="rom-title d-flex transition-fast-in-fast-out bg-tooltip text-caption">
-                <v-list-item>{{ rom.file_name }}</v-list-item>
-            </div>
-        </v-expand-transition>
-        <v-chip-group class="pl-1 pt-0">
-            <v-chip
-                v-show="rom.region"
-                size="x-small"
-                class="bg-chip"
-                label>
-                {{ rom.region }}
-            </v-chip>
-            <v-chip
-                v-show="rom.revision"
-                size="x-small"
-                class="bg-chip"
-                label>
-                {{ rom.revision }}
-            </v-chip>
-        </v-chip-group>
-    </v-img>
+    <router-link
+        style="text-decoration: none; color: inherit;"
+        :to="`/${$route.params.platform}/roms/${rom.file_name}`">
+        <v-img
+            :value="rom.file_name"
+            :key="rom.file_name"
+            v-bind="hoverProps"
+            :src="'/assets'+rom.path_cover_l+'?reload='+forceImgReload"
+            :lazy-src="'/assets'+rom.path_cover_s+'?reload='+forceImgReload"
+            class="cover"
+            cover>
+            <template v-slot:placeholder>
+                <div class="d-flex align-center justify-center fill-height">
+                    <v-progress-circular color="secondary" indeterminate/>
+                </div>
+            </template>
+            <v-expand-transition>
+                <div 
+                    v-if="isHovering || !rom.has_cover"
+                    class="rom-title d-flex transition-fast-in-fast-out bg-tooltip text-caption">
+                    <v-list-item>{{ rom.file_name }}</v-list-item>
+                </div>
+            </v-expand-transition>
+            <v-chip-group class="pl-1 pt-0">
+                <v-chip
+                    v-show="rom.region"
+                    size="x-small"
+                    class="bg-chip"
+                    label>
+                    {{ rom.region }}
+                </v-chip>
+                <v-chip
+                    v-show="rom.revision"
+                    size="x-small"
+                    class="bg-chip"
+                    label>
+                    {{ rom.revision }}
+                </v-chip>
+            </v-chip-group>
+        </v-img>
+    </router-link>
 </template>
 
 <style scoped>
