40e98d9d3ff2664abb5e671a2c0229d492570120
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-18 16:56:52 +0200

    home route view added

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Info from '@/components/Home/Info.vue'
+</script>
+
+<template>
+
+    <v-row class="justify-content align-center">
+        <info/>
+    </v-row>
+
+</template>
+
+<style scoped>
+</style>
