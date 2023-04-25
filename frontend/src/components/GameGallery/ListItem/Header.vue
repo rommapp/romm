ad256613809b8cf7728af7c7f76f77368a141ac6
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-25 19:33:44 +0200

    download progress bar added to list view

--- frontend/src/components/GameGallery/ListItem/Header.vue
+++ frontend/src/components/GameGallery/ListItem/Header.vue
@@ -1,22 +1,26 @@
 <template>
     <thead>
-        <v-list-item>
-            <v-row class="text-subtitle-2">
-                <v-col cols="9" xs="9" sm="6" md="3" lg="3"><span>Name</span></v-col>
-                <v-col md="4" lg="4" class="hidden-sm-and-down"><span>File</span></v-col>
-                <v-col md="1" lg="1" class="hidden-sm-and-down"><span>Platform</span></v-col>
-                <v-col xs="2" sm="2" md="2" lg="2" class="hidden-xs"><span>Size</span></v-col>
-                <v-col xs="1" sm="1" md="1" lg="1" class="hidden-xs"><span>Region</span></v-col>
-                <v-col xs="1" sm="1" md="1" lg="1" class="hidden-xs"><span>Revision</span></v-col>
-            </v-row>
-            <template v-slot:prepend>
-                <v-avatar></v-avatar>
-            </template>
-            <template v-slot:append>
+        <v-row>
+            <v-col>
+            <v-list-item>
+                <v-row class="text-subtitle-2">
+                    <v-col cols="9" xs="9" sm="6" md="3" lg="3"><span>Name</span></v-col>
+                    <v-col md="4" lg="4" class="hidden-sm-and-down"><span>File</span></v-col>
+                    <v-col md="1" lg="1" class="hidden-sm-and-down"><span>Platform</span></v-col>
+                    <v-col xs="2" sm="2" md="2" lg="2" class="hidden-xs"><span>Size</span></v-col>
+                    <v-col xs="1" sm="1" md="1" lg="1" class="hidden-xs"><span>Reg</span></v-col>
+                    <v-col xs="1" sm="1" md="1" lg="1" class="hidden-xs"><span>Rev</span></v-col>
+                </v-row>
+                <template v-slot:prepend>
+                    <v-avatar></v-avatar>
+                </template>
+            </v-list-item>
+            </v-col>
+            <v-col cols="2" xs="2" sm="2" md="1" lg="1" class="d-flex justify-center align-center mr-6">
                 <v-btn size="x-small" variant="text"/>
                 <v-btn size="x-small" variant="text"/>
-                <v-btn size="x-small" variant="text" class="hidden-sm-and-down"/>
-            </template>
-        </v-list-item>
+                <v-btn size="x-small" variant="text"/>
+            </v-col>
+        </v-row>
     </thead>
 </template>
--- frontend/src/components/GameGallery/ListItem/Item.vue
+++ frontend/src/components/GameGallery/ListItem/Item.vue
@@ -1,39 +1,46 @@
 <script setup>
 import { ref, inject } from 'vue'
 import { downloadRom, downloadSave } from '@/services/download.js'
+import { storeDownloader } from '@/stores/downloader.js'
 
 // Props
 const props = defineProps(['rom'])
 const forceImgReload = Date.now()
 const saveFiles = ref(false)
+const downloader = storeDownloader()
 
 // Event listeners bus
 const emitter = inject('emitter')
 </script>
 
 <template>
-    <v-list-item 
-        :to="`/platform/${$route.params.platform}/rom/${rom.id}`"
-        :value="rom.id"
-        :key="rom.id">
-        <v-row class="text-subtitle-2">
-            <v-col cols="9" xs="9" sm="6" md="3" lg="3"><span>{{ rom.r_name }}</span></v-col>
-            <v-col md="4" lg="4" class="hidden-sm-and-down"><span>{{ rom.file_name }}</span></v-col>
-            <v-col md="1" lg="1" class="hidden-sm-and-down"><span>{{ rom.p_slug }}</span></v-col>
-            <v-col xs="2" sm="2" md="2" lg="2" class="hidden-xs"><span>{{ rom.file_size }} {{ rom.file_size_units }}</span></v-col>
-            <v-col xs="1" sm="1" md="1" lg="1" class="hidden-xs"><span>{{ rom.region }}</span></v-col>
-            <v-col xs="1" sm="1" md="1" lg="1" class="hidden-xs"><span>{{ rom.revision }}</span></v-col>
-        </v-row>
-        
-        <template v-slot:prepend>
-            <v-avatar :rounded="0">
-                <v-img
-                :src="'/assets'+rom.path_cover_l+'?reload='+forceImgReload"
-                :lazy-src="'/assets'+rom.path_cover_s+'?reload='+forceImgReload"
-                min-height="100"/>
-            </v-avatar>
-        </template>
-        <template v-slot:append>
+    <v-row>
+        <v-col>
+            <v-list-item 
+                :to="`/platform/${$route.params.platform}/rom/${rom.id}`"
+                :value="rom.id"
+                :key="rom.id">
+                <v-row class="text-subtitle-2">
+                    <v-col cols="9" xs="9" sm="6" md="3" lg="3"><span>{{ rom.r_name }}</span></v-col>
+                    <v-col md="4" lg="4" class="hidden-sm-and-down"><span>{{ rom.file_name }}</span></v-col>
+                    <v-col md="1" lg="1" class="hidden-sm-and-down"><span>{{ rom.p_slug }}</span></v-col>
+                    <v-col xs="2" sm="2" md="2" lg="2" class="hidden-xs"><span>{{ rom.file_size }} {{ rom.file_size_units }}</span></v-col>
+                    <v-col xs="1" sm="1" md="1" lg="1" class="hidden-xs"><span>{{ rom.region }}</span></v-col>
+                    <v-col xs="1" sm="1" md="1" lg="1" class="hidden-xs"><span>{{ rom.revision }}</span></v-col>
+                </v-row>
+                
+                <template v-slot:prepend>
+                    <v-avatar :rounded="0">
+                        <v-progress-linear color="rommAccent1" :active="downloader.value.includes(rom.file_name)" :indeterminate="true" absolute/>
+                        <v-img
+                        :src="'/assets'+rom.path_cover_s+'?reload='+forceImgReload"
+                        :lazy-src="'/assets'+rom.path_cover_s+'?reload='+forceImgReload"
+                        min-height="150"/>
+                    </v-avatar>
+                </template>
+            </v-list-item>
+        </v-col>
+        <v-col cols="2" xs="2" sm="2" md="1" lg="1" class="d-flex justify-center align-center mr-6">
             <v-btn
                 @click="downloadRom(rom, emitter)"
                 icon="mdi-download"
@@ -50,9 +57,7 @@ const emitter = inject('emitter')
                 icon="mdi-dots-vertical"
                 size="x-small"
                 variant="text"
-                class="hidden-sm-and-down"
                 :disabled="!saveFiles"/>
-        </template>
-            
-    </v-list-item>
+        </v-col>
+    </v-row>
 </template>
