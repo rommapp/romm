3b6d2646b051db58f3236b6f0c95a53a37376022
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-25 18:37:46 +0200

    n_roms chip changed to label

--- frontend/src/components/MainDrawer/Platform.vue
+++ frontend/src/components/MainDrawer/Platform.vue
@@ -7,12 +7,12 @@ const props = defineProps(['platform', 'rail'])
 
 <template>
     <v-list-item :to="`/platform/${platform.slug}`" :value="platform.slug" :key="platform" class="pt-4 pb-4 bg-terciary">
-        <p class="text-body-2 text-truncate">{{ rail ? '' : platform.name }}</p>
+        <span class="text-body-2 text-truncate">{{ rail ? '' : platform.name }}</span>
         <template v-slot:prepend>
             <v-avatar :rounded="0" size="40"><v-img :src="'/assets/platforms/'+platform.slug+'.ico'"></v-img></v-avatar>
         </template>
         <template v-slot:append>
-            <v-chip class="ml-4 bg-chip" size="small">{{ platform.n_roms }}</v-chip>
+            <v-chip class="ml-4 bg-chip" size="x-small" label>{{ platform.n_roms }}</v-chip>
         </template>
     </v-list-item>
 </template>
