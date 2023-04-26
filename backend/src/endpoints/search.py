d0aaea695cb2db5d72253ea564ffc7323af9f424
zurdi zurdo <zurdizurdo25@gmail.com>
2023-04-26 11:20:00 +0200

    igdb api calls refactored

--- backend/src/endpoints/search.py
+++ backend/src/endpoints/search.py
@@ -16,7 +16,7 @@ async def search_rom_igdb(req: Request, igdb_id: str = None) -> dict:
     log.info(emoji.emojize(":magnifying_glass_tilted_right: IGDB Searching"))
     if igdb_id:
         log.info(f"Searching by id: {igdb_id}")
-        matched_roms = igdbh.get_matched_roms_by_id(igdb_id)
+        matched_roms = igdbh.get_matched_rom_by_id(igdb_id)
     else:
         log.info(emoji.emojize(f":video_game: {rom['p_slug']}: {COLORS['orange']}{rom['file_name']}{COLORS['reset']}"))
         matched_roms = igdbh.get_matched_roms(rom['file_name'], rom['p_igdb_id'], rom['p_slug'])
