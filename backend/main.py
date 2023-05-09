7713cbb1cc7590c55a64a295b98952afe3f47c22
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-05-09 09:49:49 -0400

    Complete rewrite of backend

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
+import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
+from dotenv import find_dotenv, load_dotenv
 
-from config import DEV_PORT, DEV_HOST
+load_dotenv(find_dotenv())
+
 from endpoints import scan, search, platform, rom
 
 app = FastAPI()
@@ -18,12 +20,12 @@ app.include_router(search.router)
 app.include_router(platform.router)
 app.include_router(rom.router)
 
-
 @app.on_event("startup")
 def startup() -> None:
     """Startup application."""
     pass
 
-
 if __name__ == '__main__':
+    from config import DEV_PORT, DEV_HOST
+
     uvicorn.run("main:app", host=DEV_HOST, port=DEV_PORT, reload=True)
