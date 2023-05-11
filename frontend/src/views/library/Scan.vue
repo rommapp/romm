89ac81c675319dbc1a604b0ab9dbb94276a26a57
zurdi zurdo <zurdizurdo25@gmail.com>
2023-05-11 18:43:52 +0200

    scan log added in frontend

--- frontend/src/views/library/Scan.vue
+++ frontend/src/views/library/Scan.vue
@@ -5,10 +5,11 @@ import { storePlatforms } from '@/stores/platforms.js'
 import { storeScanning } from '@/stores/scanning.js'
 
 // Props
-const wsMsg = ref("")
 const platforms = storePlatforms()
 const platformsToScan = ref([])
 const scanning = storeScanning()
+const scanningPlatform = ref("")
+const scannedRoms = ref([])
 const completeRescan = ref(false)
 
 // Event listeners bus
@@ -17,14 +18,12 @@ const emitter = inject('emitter')
 // Functions
 async function scan() {
     scanning.set(true);
-    wsMsg.value = 'Scanning...'
     const socket = io({ path: '/ws/socket.io/', transports: ['websocket', 'polling'] })
-    socket.on("connect", () => {console.log("ws connected")})
-    socket.on('disconnect', () => {console.log('ws disconnected');});
-    socket.on("scanning", (params) => {wsMsg.value = 'Scanning > '+params['platform']+' - '+params['rom']})
-    socket.on("done", (msg) => { 
-        wsMsg.value = msg
+    socket.on("scanning_platform", (platform) => { scanningPlatform.value = platform })
+    socket.on("scanning_rom", (rom) => { scannedRoms.value.push(rom) })
+    socket.on("done", () => {
         scanning.set(false);
+        // emitter.emit('refresh')
         emitter.emit('snackbarScan', {'msg': "Scan completed successfully!", 'icon': 'mdi-check-bold', 'color': 'green'})
         socket.close()
     })
@@ -60,7 +59,7 @@ async function scan() {
                 persistent-hint/>
         </v-row>
 
-        <v-row no-gutters>
+        <v-row class="mb-4" no-gutters>
             <v-btn
                 @click="scan()"
                 :disabled="scanning.value"
@@ -77,7 +76,16 @@ async function scan() {
             </v-btn>
         </v-row>
 
-        <div class="ma-5"><span class="text-body-1">{{ wsMsg }}</span></div>
+        <v-row no-gutters class="align-center">
+            <v-col cols="12" class="pa-2">
+                <v-avatar :rounded="0" size="40"><v-img :src="'/assets/platforms/'+scanningPlatform+'.ico'"></v-img></v-avatar>
+                <span class="text-overline ml-5"> {{ scanningPlatform }}</span>
+            </v-col>
+            <v-col cols="12">
+                <v-list-item v-for="rom in scannedRoms" class="text-body-2" disabled> - {{ rom }}</v-list-item>
+                <!-- <v-list-item class="text-body-2" disabled>{{ scanningRom }}</v-list-item> -->
+            </v-col>
+        </v-row>
     </div>
 
 </template>
