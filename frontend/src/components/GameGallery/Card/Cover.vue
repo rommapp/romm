97ea420af1fe0ac95c6da70ef083b367f6eceb4b
zurdi zurdo <zurdizurdo25@gmail.com>
2023-05-15 17:03:11 +0200

    roms gallery routes fixed

--- frontend/src/components/GameGallery/Card/Cover.vue
+++ frontend/src/components/GameGallery/Card/Cover.vue
@@ -6,7 +6,7 @@ const props = defineProps(['rom', 'isHovering', 'hoverProps', 'size'])
 <template>
     <router-link
         style="text-decoration: none; color: inherit;"
-        :to="`/platform/${$route.params.platform}/rom/${rom.id}`">
+        :to="`/platform/${$route.params.platform}/roms/${rom.id}`">
         <v-img
             :value="rom.id"
             :key="rom.id"
