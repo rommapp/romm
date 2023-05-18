322a22c152a9e1ee6f1cdf8b455c91d3de21f48e
zurdi zurdo <zurdizurdo25@gmail.com>
2023-05-18 17:25:09 +0200

    views code cleaned

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -13,7 +13,7 @@ const { lgAndUp } = useDisplay()
 
 <template>
 
-        <v-row id="home-header-logo" class="ma-4" no-gutters>
+        <v-row id="home-header-logo" class="pa-2" no-gutters>
             <v-spacer/>
             <v-col cols="12" xs="12" sm="10" md="10" lg="10">
                 <v-img :height="lgAndUp ? 200 : 150" src="/assets/romm_complete.svg" cover />
@@ -21,7 +21,7 @@ const { lgAndUp } = useDisplay()
             <v-spacer/>
         </v-row>
 
-        <v-row id="home-chips" class="ma-4" no-gutters>
+        <v-row id="home-chips" class="pa-2" no-gutters>
             <v-spacer/>
             <v-col cols="12" xs="12" sm="10" md="10" lg="10" class="d-flex justify-center">
                 <v-chip-group>
@@ -36,13 +36,13 @@ const { lgAndUp } = useDisplay()
             <v-spacer/>
         </v-row>
 
-        <v-row id="platforms-title" class="ma-4" no-gutters>
+        <v-row id="platforms-title" class="pa-2" no-gutters>
             <v-avatar :rounded="0" size="auto"><v-icon>mdi-controller</v-icon></v-avatar>
             <span class="text-h6 ml-2">Platforms</span>
             <v-divider class="border-opacity-25"/>
         </v-row>
 
-        <v-row id="platforms-cards" class="ma-4" no-gutters>
+        <v-row id="platforms-cards" class="pa-2" no-gutters>
             <v-col v-for="platform in platforms.value" class="pr-1 pl-1" :key="platform.slug"
                 :cols="views[0]['size-cols']" :xs="views[0]['size-xs']" :sm="views[0]['size-sm']" :md="views[0]['size-md']" :lg="views[0]['size-lg']">
                 <platform :platform="platform" :key="platform.slug"/>
