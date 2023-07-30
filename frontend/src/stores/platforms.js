c7cbf8e73e929580c419fc29e52751a2f29e883d
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-07-30 17:35:35 -0400

    Refactor stores

--- frontend/src/stores/platforms.js
+++ frontend/src/stores/platforms.js
@@ -1,16 +1,22 @@
 import { defineStore } from "pinia";
 
 export default defineStore("platforms", {
-  state: () => ({ value: [] }),
-
+  state: () => {
+    return {
+      platforms: []
+    };
+  },
+  getters: {
+    totalGames: ({ platforms }) => {
+      debugger;
+      platforms?.reduce((count, p) => {
+        return count + p.n_roms;
+      }, 0) ?? 0;
+    },
+  },
   actions: {
     set(platforms) {
-      this.value = platforms;
-    },
-    getTotalGames() {
-      return this.value.reduce((count, p) => {
-        return count + p.n_roms;
-      }, 0);
+      this.platforms = platforms;
     },
   },
 });
