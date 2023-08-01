0445a41c82125120ac0d6f78c57f8cc693347ebf
zurdi <zurdizurdo25@gmail.com>
2023-08-01 10:24:05 +0200

    platforms refresh fixed after scan

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,7 +21,7 @@ const emitter = inject("emitter");
 emitter.on("refreshPlatforms", async () => {
   try {
     const { data } = await fetchPlatformsApi();
-    platforms.set(data.data);
+    platforms.set(data);
   } catch (error) {
     console.error("Couldn't fetch platforms:", error);
   } finally {
