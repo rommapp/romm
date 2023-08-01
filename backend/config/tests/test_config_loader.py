fb4d644fb21cf77650ec05726b4aa83248e9d5bc
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-08-01 09:45:21 -0400

    Fix broken tests from merge

--- backend/config/tests/test_config_loader.py
+++ backend/config/tests/test_config_loader.py
@@ -1,25 +1,30 @@
+import os
+from pathlib import Path
+
 from config.config_loader import ConfigLoader
 
 
 def test_config_loader():
-    cl = ConfigLoader("backend/config/tests/fixtures/config.yml")
+    loader = ConfigLoader(
+        os.path.join(Path(__file__).resolve().parent, "fixtures", "config.yml")
+    )
 
-    assert cl.config["EXCLUDED_PLATFORMS"] == ["romm"]
-    assert cl.config["EXCLUDED_EXTENSIONS"] == ["xml"]
-    assert cl.config["EXCLUDED_FILES"] == ["info.txt"]
-    assert cl.config["EXCLUDED_MULTI_FILES"] == ["my_multi_file_game", "DLC"]
-    assert cl.config["EXCLUDED_MULTI_PARTS_EXT"] == ["txt"]
-    assert cl.config["EXCLUDED_MULTI_PARTS_FILES"] == ["data.xml"]
-    assert cl.config["PLATFORMS_BINDING"] == {"gc": "ngc", "psx": "ps"}
+    assert loader.config["EXCLUDED_PLATFORMS"] == ["romm"]
+    assert loader.config["EXCLUDED_EXTENSIONS"] == ["xml"]
+    assert loader.config["EXCLUDED_FILES"] == ["info.txt"]
+    assert loader.config["EXCLUDED_MULTI_FILES"] == ["my_multi_file_game", "DLC"]
+    assert loader.config["EXCLUDED_MULTI_PARTS_EXT"] == ["txt"]
+    assert loader.config["EXCLUDED_MULTI_PARTS_FILES"] == ["data.xml"]
+    assert loader.config["PLATFORMS_BINDING"] == {"gc": "ngc", "psx": "ps"}
 
 
 def test_empty_config_loader():
-    cl = ConfigLoader("")
+    loader = ConfigLoader("")
 
-    assert cl.config.get("EXCLUDED_PLATFORMS") == []
-    assert cl.config.get("EXCLUDED_EXTENSIONS") == []
-    assert cl.config.get("EXCLUDED_FILES") == []
-    assert cl.config.get("EXCLUDED_MULTI_FILES") == []
-    assert cl.config.get("EXCLUDED_MULTI_PARTS_EXT") == []
-    assert cl.config.get("EXCLUDED_MULTI_PARTS_FILES") == []
-    assert cl.config.get("PLATFORMS_BINDING") == {}
+    assert loader.config.get("EXCLUDED_PLATFORMS") == []
+    assert loader.config.get("EXCLUDED_EXTENSIONS") == []
+    assert loader.config.get("EXCLUDED_FILES") == []
+    assert loader.config.get("EXCLUDED_MULTI_FILES") == []
+    assert loader.config.get("EXCLUDED_MULTI_PARTS_EXT") == []
+    assert loader.config.get("EXCLUDED_MULTI_PARTS_FILES") == []
+    assert loader.config.get("PLATFORMS_BINDING") == {}
