aa1d13f74c31e7c8339a3a6193d27616e6efb1ff
zurdi <zurdizurdo25@gmail.com>
2023-08-01 13:45:31 +0200

    code refactor and styled

--- backend/utils/fs.py
+++ backend/utils/fs.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 import datetime
-import requests
 from pathlib import Path
+import requests
 
 from config import (
     LIBRARY_BASE_PATH,
@@ -35,8 +35,9 @@ def _cover_exists(p_slug: str, file_name: str, size: str) -> bool:
     Returns
         True if cover exists in filesystem else False
     """
-    logo_path: str = f"{RESOURCES_BASE_PATH}/{p_slug}/{file_name}/cover/{size}.png"
-    return True if os.path.exists(logo_path) else False
+    return bool(
+        os.path.exists(f"{RESOURCES_BASE_PATH}/{p_slug}/{file_name}/cover/{size}.png")
+    )
 
 
 def _store_cover(p_slug: str, file_name: str, url_cover: str, size: str) -> None:
@@ -50,7 +51,9 @@ def _store_cover(p_slug: str, file_name: str, url_cover: str, size: str) -> None
     """
     cover_file: str = f"{size}.png"
     cover_path: str = f"{RESOURCES_BASE_PATH}/{p_slug}/{file_name}/cover"
-    res = requests.get(url_cover.replace("t_thumb", f"t_cover_{size}"), stream=True)
+    res = requests.get(
+        url_cover.replace("t_thumb", f"t_cover_{size}"), stream=True, timeout=120
+    )
     if res.status_code == 200:
         Path(cover_path).mkdir(parents=True, exist_ok=True)
         with open(f"{cover_path}/{cover_file}", "wb") as f:
@@ -68,7 +71,7 @@ def _get_cover_path(p_slug: str, file_name: str, size: str) -> str:
     return f"{p_slug}/{file_name}/cover/{size}.png?timestamp={str(datetime.datetime.now().timestamp())}"
 
 
-def get_cover(overwrite: bool, p_slug: str, file_name: str, url_cover: str) -> tuple:
+def get_cover(overwrite: bool, p_slug: str, file_name: str, url_cover: str) -> dict:
     # Cover small
     if (overwrite or not _cover_exists(p_slug, file_name, "small")) and url_cover:
         _store_cover(p_slug, file_name, url_cover, "small")
@@ -102,7 +105,7 @@ def _store_screenshot(p_slug: str, file_name: str, url: str, idx: int) -> None:
     """
     screenshot_file: str = f"{idx}.jpg"
     screenshot_path: str = f"{RESOURCES_BASE_PATH}/{p_slug}/{file_name}/screenshots"
-    res = requests.get(url, stream=True)
+    res = requests.get(url, stream=True, timeout=120)
     if res.status_code == 200:
         Path(screenshot_path).mkdir(parents=True, exist_ok=True)
         with open(f"{screenshot_path}/{screenshot_file}", "wb") as f:
@@ -120,17 +123,17 @@ def _get_screenshot_path(p_slug: str, file_name: str, idx: str) -> str:
     return f"{p_slug}/{file_name}/screenshots/{idx}.jpg"
 
 
-def get_screenshots(p_slug: str, file_name: str, url_screenshots: list) -> tuple:
+def get_screenshots(p_slug: str, file_name: str, url_screenshots: list) -> dict:
     path_screenshots: list[str] = []
     for idx, url in enumerate(url_screenshots):
         _store_screenshot(p_slug, file_name, url, idx)
-        path_screenshots.append(_get_screenshot_path(p_slug, file_name, idx))
+        path_screenshots.append(_get_screenshot_path(p_slug, file_name, str(idx)))
     return {"path_screenshots": path_screenshots}
 
 
 def store_default_resources() -> None:
     """Store default cover resources in the filesystem"""
-    defaul_covers: dict = [
+    defaul_covers: list = [
         {"url": DEFAULT_URL_COVER_L, "size": "big"},
         {"url": DEFAULT_URL_COVER_S, "size": "small"},
     ]
@@ -141,7 +144,7 @@ def store_default_resources() -> None:
 
 # ========= Platforms utils =========
 def _exclude_platforms(platforms) -> None:
-    [
+    _ = [
         platforms.remove(excluded)
         for excluded in cl.config["EXCLUDED_PLATFORMS"]
         if excluded in platforms
@@ -161,13 +164,13 @@ def get_platforms() -> list[str]:
             else list(os.walk(LIBRARY_BASE_PATH))[0][1]
         )
         _exclude_platforms(platforms)
-    except IndexError:
-        raise PlatformsNotFoundException
+    except IndexError as exc:
+        raise PlatformsNotFoundException from exc
     return platforms
 
 
 # ========= Roms utils =========
-def get_roms_structure(p_slug: str) -> tuple:
+def get_roms_structure(p_slug: str) -> str:
     return (
         f"roms/{p_slug}"
         if os.path.exists(HIGH_PRIO_STRUCTURE_PATH)
@@ -175,9 +178,9 @@ def get_roms_structure(p_slug: str) -> tuple:
     )
 
 
-def _exclude_files(files, type) -> list[str]:
-    excluded_extensions = cl.config[f"EXCLUDED_{type.upper()}_EXT"]
-    excluded_names = cl.config[f"EXCLUDED_{type.upper()}_FILES"]
+def _exclude_files(files, filetype) -> list[str]:
+    excluded_extensions = cl.config[f"EXCLUDED_{filetype.upper()}_EXT"]
+    excluded_names = cl.config[f"EXCLUDED_{filetype.upper()}_FILES"]
     filtered_files: list = []
     for file in files:
         if file.split(".")[-1] in excluded_extensions or file in excluded_names:
@@ -199,7 +202,7 @@ def _exclude_multi_roms(roms) -> list[str]:
 def get_rom_files(rom: str, roms_path: str) -> list[str]:
     rom_files: list = []
     for path, _, files in os.walk(f"{roms_path}/{rom}"):
-        [
+        _ = [
             rom_files.append(f"{Path(path, f)}".replace(f"{roms_path}/{rom}/", ""))
             for f in _exclude_files(files, "multi_parts")
         ]
@@ -217,19 +220,19 @@ def get_roms(p_slug: str) -> list[dict] or int:
     roms_file_path = f"{LIBRARY_BASE_PATH}/{roms_path}"
     try:
         fs_single_roms: list[str] = list(os.walk(roms_file_path))[0][2]
-    except IndexError:
-        raise RomsNotFoundException(p_slug)
+    except IndexError as exc:
+        raise RomsNotFoundException(p_slug) from exc
     try:
         fs_multi_roms: list[str] = list(os.walk(roms_file_path))[0][1]
-    except IndexError:
-        raise RomsNotFoundException(p_slug)
+    except IndexError as exc:
+        raise RomsNotFoundException(p_slug) from exc
     fs_roms: list[dict] = [
         {"multi": False, "file_name": rom}
         for rom in _exclude_files(fs_single_roms, "single")
     ] + [
         {"multi": True, "file_name": rom} for rom in _exclude_multi_roms(fs_multi_roms)
     ]
-    [
+    _ = [
         rom.update({"files": get_rom_files(rom["file_name"], roms_file_path)})
         for rom in fs_roms
     ]
@@ -242,7 +245,7 @@ def get_rom_size(multi: bool, rom: str, files: list, roms_path: str) -> str:
         if not multi
         else [f"{LIBRARY_BASE_PATH}/{roms_path}/{rom}/{file}" for file in files]
     )
-    total_size: int = 0
+    total_size: float = 0.0
     for file in files:
         total_size += os.stat(file).st_size
     for unit in ["B", "KB", "MB", "GB", "TB", "PB"]:
@@ -262,10 +265,7 @@ def _rom_exists(p_slug: str, file_name: str) -> bool:
         True if rom exists in filesystem else False
     """
     rom_path = get_roms_structure(p_slug)
-    exists: bool = (
-        True if os.path.exists(f"{LIBRARY_BASE_PATH}/{rom_path}/{file_name}") else False
-    )
-    return exists
+    return bool(os.path.exists(f"{LIBRARY_BASE_PATH}/{rom_path}/{file_name}"))
 
 
 def rename_rom(p_slug: str, old_name: str, new_name: str) -> None:
@@ -286,5 +286,5 @@ def remove_rom(p_slug: str, file_name: str) -> None:
             os.remove(f"{LIBRARY_BASE_PATH}/{rom_path}/{file_name}")
         except IsADirectoryError:
             shutil.rmtree(f"{LIBRARY_BASE_PATH}/{rom_path}/{file_name}")
-    except FileNotFoundError:
-        raise RomNotFoundError(file_name, p_slug)
+    except FileNotFoundError as exc:
+        raise RomNotFoundError(file_name, p_slug) from exc
