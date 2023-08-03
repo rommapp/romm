83f69f143b1312dce521dd347d7a825fa36803bc
zurdi <zurdizurdo25@gmail.com>
2023-08-03 17:16:26 +0200

    typo

--- backend/handler/igdb_handler.py
+++ backend/handler/igdb_handler.py
@@ -258,7 +258,7 @@ class TwitchAuth:
 
     def get_oauth_token(self) -> str:
         if not self._is_token_valid():
-            log.warning("Twitch token invalid: fetching a new one…")
+            log.warning("Twitch token invalid: fetching a new one...")
             self._update_twitch_token()
 
         return self.token
