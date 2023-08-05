dc898358bb52d47a1ce7f0a475f158e4be19eb5d
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-08-04 23:24:41 -0400

    Store twitck token in redis cache + switch back to single task

--- backend/endpoints/scan.py
+++ backend/endpoints/scan.py
@@ -14,53 +14,57 @@ from worker import redis_conn, redis_url
 scan_queue = Queue(connection=redis_conn)
 
 
-async def scan_platform(p_slug: str, complete_rescan: bool):
+async def scan_platforms(paltforms: str, complete_rescan: bool):
     # Connect to external socketio server
     sm = socketio.AsyncRedisManager(redis_url, write_only=True)
 
+    # Scanning file system
     try:
-        # Verify that platform exists
-        scanned_platform = fastapi.scan_platform(p_slug)
-    except RomsNotFoundException as e:
+        fs_platforms: list[str] = fs.get_platforms()
+    except PlatformsNotFoundException as e:
         log.error(e)
+        await socket_server.emit("scan:done_ko", e.message)
         return
 
-    await sm.emit(
-        "scan:scanning_platform",
-        {"p_name": scanned_platform.name, "p_slug": scanned_platform.slug},
-    )
+    platform_list = paltforms.split(",") if paltforms else fs_platforms
+    for p_slug in platform_list:
+        try:
+            # Verify that platform exists
+            scanned_platform = fastapi.scan_platform(p_slug)
+        except RomsNotFoundException as e:
+            log.error(e)
+            return
 
-    dbh.add_platform(scanned_platform)
-
-    # Scanning roms
-    fs_roms: list[str] = fs.get_roms(scanned_platform.fs_slug)
-    for rom in fs_roms:
-        rom_id: int = dbh.rom_exists(scanned_platform.slug, rom["file_name"])
-        if rom_id and not complete_rescan:
-            continue
-
-        scanned_rom: Rom = fastapi.scan_rom(scanned_platform, rom)
         await sm.emit(
-            "scan:scanning_rom",
-            {
-                "p_slug": scanned_platform.slug,
-                "p_name": scanned_platform.name,
-                "file_name": scanned_rom.file_name,
-                "r_name": scanned_rom.r_name,
-            },
+            "scan:scanning_platform",
+            {"p_name": scanned_platform.name, "p_slug": scanned_platform.slug},
         )
 
-        if rom_id:
-            scanned_rom.id = rom_id
+        dbh.add_platform(scanned_platform)
 
-        dbh.add_rom(scanned_rom)
-    dbh.purge_roms(scanned_platform.slug, [rom["file_name"] for rom in fs_roms])
+        # Scanning roms
+        fs_roms: list[str] = fs.get_roms(scanned_platform.fs_slug)
+        for rom in fs_roms:
+            rom_id: int = dbh.rom_exists(scanned_platform.slug, rom["file_name"])
+            if rom_id and not complete_rescan:
+                continue
 
+            scanned_rom: Rom = fastapi.scan_rom(scanned_platform, rom)
+            await sm.emit(
+                "scan:scanning_rom",
+                {
+                    "p_slug": scanned_platform.slug,
+                    "p_name": scanned_platform.name,
+                    "file_name": scanned_rom.file_name,
+                    "r_name": scanned_rom.r_name,
+                },
+            )
 
-async def report_complete(fs_platforms: list[str]):
-    # Connect to external socketio server
-    sm = socketio.AsyncRedisManager(redis_url, write_only=True)
+            if rom_id:
+                scanned_rom.id = rom_id
 
+            dbh.add_rom(scanned_rom)
+        dbh.purge_roms(scanned_platform.slug, [rom["file_name"] for rom in fs_roms])
     dbh.purge_platforms(fs_platforms)
 
     await sm.emit("scan:done", {})
@@ -73,26 +77,5 @@ async def scan_handler(_sid: str, platforms: str, complete_rescan: bool = True):
     log.info(emoji.emojize(":magnifying_glass_tilted_right: Scanning "))
     fs.store_default_resources()
 
-    # Scanning file system
-    try:
-        fs_platforms: list[str] = fs.get_platforms()
-    except PlatformsNotFoundException as e:
-        log.error(e)
-        await socket_server.emit("scan:done_ko", e.message)
-        return
-
-    platform_list = platforms.split(",") if platforms else fs_platforms
-
     # Queue up a job for each platform
-    jobs = scan_queue.enqueue_many(
-        [
-            Queue.prepare_data(
-                scan_platform,
-                (platform, complete_rescan),
-                job_id=f"scan_platform_{platform}",
-            )
-            for platform in platform_list
-        ]
-    )
-
-    scan_queue.enqueue(report_complete, fs_platforms, depends_on=jobs)
+    scan_queue.enqueue(scan_platforms, platforms, complete_rescan)
