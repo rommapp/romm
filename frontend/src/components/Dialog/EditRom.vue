3d50b310e6a830d6645c4776799a492ca7ed07a9
zurdi <zurdizurdo25@gmail.com>
2023-08-08 00:39:06 +0200

    edit rom dialog added

---
+++ frontend/src/components/Dialog/EditRom.vue
@@ -0,0 +1,154 @@
+<script setup>
+import { ref, inject } from "vue";
+import { useRouter } from "vue-router";
+import { useDisplay } from "vuetify";
+import { deleteRomApi } from "@/services/api.js";
+
+const { xs, mdAndDown, lgAndUp } = useDisplay();
+const props = defineProps(["show", "rom", "updatedRom"]);
+const emitter = inject("emitter");
+const updatedRom = ref(undefined);
+const saveFiles = ref(false);
+const updating = ref(false);
+const loading = ref(true);
+
+async function updateRom(updatedData = { ...updatedRom.value }) {
+  emitter.emit("close-edit-dialog");
+  emitter.emit("updating-rom-start");
+
+  await updateRomApi(rom.value, {
+    r_igdb_id: updatedData.r_igdb_id,
+    r_slug: updatedData.r_slug,
+    summary: updatedData.summary,
+    url_cover: updatedData.url_cover,
+    url_screenshots: updatedData.url_screenshots,
+    r_name: updatedData.r_name,
+    file_name: renameAsIGDB.value
+      ? updatedRom.value.file_name.replace(
+          updatedRom.value.file_name_no_tags,
+          updatedData.r_name
+        )
+      : updatedData.file_name,
+  })
+    .then((response) => {
+      rom.value = response.data.rom;
+      updatedRom.value = { ...response.data.rom };
+      emitter.emit("snackbarShow", {
+        msg: response.data.msg,
+        icon: "mdi-check-bold",
+        color: "green",
+      });
+      emitter.emit("refreshGallery");
+    })
+    .catch((error) => {
+      emitter.emit("snackbarShow", {
+        msg: error.response.data.detail,
+        icon: "mdi-close-circle",
+        color: "red",
+      });
+    });
+  emitter.emit("updating-rom-end");
+  emitter.emit("close-edit-dialog");
+}
+</script>
+
+<template>
+  <v-dialog
+    :modelValue="show"
+    scroll-strategy="none"
+    width="auto"
+    :scrim="false"
+    @click:outside="emitter.emit('close-edit-dialog')"
+    v-if="rom !== undefined"
+  >
+    <v-card
+      rounded="0"
+      :class="{
+        'edit-content': lgAndUp,
+        'edit-content-tablet': mdAndDown,
+        'edit-content-mobile': xs,
+      }"
+    >
+      <v-toolbar density="compact" class="bg-primary">
+        <v-row class="align-center" no-gutters>
+          <v-col cols="9" xs="9" sm="10" md="10" lg="11">
+            <v-icon icon="mdi-pencil-box" class="ml-5" />
+          </v-col>
+          <v-col>
+            <v-btn
+              @click="emitter.emit('close-edit-dialog')"
+              class="bg-primary"
+              rounded="0"
+              variant="text"
+              icon="mdi-close"
+              block
+            />
+          </v-col>
+        </v-row>
+      </v-toolbar>
+      <v-divider class="border-opacity-25" :thickness="1" />
+
+      <v-card-text class="bg-secondary scroll">
+        <v-row class="justify-center pa-2" no-gutters>
+          <v-text-field
+            @keyup.enter="updateRom()"
+            v-model="props.updatedRom.r_name"
+            label="Name"
+            variant="outlined"
+            required
+            hide-details
+          />
+        </v-row>
+        <v-row class="justify-center pa-2" no-gutters>
+          <v-text-field
+            @keyup.enter="updateRom()"
+            v-model="props.updatedRom.file_name"
+            label="File name"
+            variant="outlined"
+            required
+            hide-details
+          />
+        </v-row>
+        <v-row class="justify-center pa-2" no-gutters>
+          <v-textarea
+            @keyup.enter="updateRom()"
+            v-model="props.updatedRom.summary"
+            label="Summary"
+            variant="outlined"
+            required
+            hide-details
+          />
+        </v-row>
+        <v-row class="justify-center pa-2" no-gutters>
+          <v-file-input
+            @keyup.enter="updateRom()"
+            label="Custom cover [Coming soon]"
+            prepend-inner-icon="mdi-image"
+            prepend-icon=""
+            variant="outlined"
+            disabled
+            hide-details
+          />
+        </v-row>
+        <v-row class="justify-center pa-2" no-gutters>
+          <v-btn @click="emitter.emit('close-edit-dialog')">Cancel</v-btn>
+          <v-btn @click="updateRom()" class="text-rommGreen ml-5">Apply</v-btn>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<style scoped>
+.edit-content {
+  width: 900px;
+}
+
+.edit-content-tablet {
+  width: 570px;
+}
+
+.edit-content-mobile {
+  width: 85vw;
+}
+</style>
