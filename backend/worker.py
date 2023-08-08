a5c207cc80b8de18b7bf9545efef4ba39b71f01c
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-08-07 20:36:59 -0400

    Changes from self review

--- backend/worker.py
+++ backend/worker.py
@@ -6,8 +6,9 @@ from utils.cache import redis_client, redis_connectable
 listen = ["high", "default", "low"]
 
 if __name__ == "__main__":
+    # Exit if Redis is not connectable
     if not redis_connectable:
-        sys.exit(2)
+        sys.exit(0)
 
     with Connection(redis_client):
         worker = Worker(map(Queue, listen))
