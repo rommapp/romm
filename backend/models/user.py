63f45ebf08e88921f837d4135e89d04db365223d
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-08-10 00:06:25 -0400

    Working authentication via starlette

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,8 +1,8 @@
 from sqlalchemy import Column, String, Boolean, Integer
-
+from starlette.authentication import SimpleUser
 from .base import BaseModel
 
-class User(BaseModel):
+class User(SimpleUser, BaseModel):
     __tablename__ = "users"
     id = Column(Integer(), primary_key=True, autoincrement=True)
     username: str = Column(String(length=255), unique=True, index=True)
