bdc36e2eeccf911116da7bcbb76b2ac780c3054f
zurdi <zurdizurdo25@gmail.com>
2023-08-11 09:30:16 +0200

    branch name added to build local image script

--- docker/build_local_image.sh
+++ docker/build_local_image.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 VERSION=$(cat .romm-version)
-docker build -t zurdi15/romm:local-${VERSION} . --file ./docker/Dockerfile
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
+branch_name=${branch_name##refs/heads/}
+docker build -t zurdi15/romm:local-${VERSION}-${branch_name} . --file ./docker/Dockerfile
