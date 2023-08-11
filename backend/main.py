99871e4bc3a49f38cde6ba17b5ce57f0bb8b7d83
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-08-11 10:47:12 -0400

    Split oauth endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,18 @@
 import uvicorn
 import alembic.config
+import re
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_pagination import add_pagination
 from starlette.middleware.authentication import AuthenticationMiddleware
 from starlette.middleware.sessions import SessionMiddleware
-from starlette_csrf import CSRFMiddleware
 
 from config import DEV_PORT, DEV_HOST, SECRET_KEY
-from endpoints import search, platform, rom, identity, scan  # noqa
+from endpoints import search, platform, rom, identity, oauth, scan  # noqa
 from utils.socket import socket_app
-from utils.auth import BasicAuthBackend
+from utils.auth import BasicAuthBackend, CustomCSRFMiddleware
+
+
 
 app = FastAPI()
 
@@ -32,10 +34,12 @@ app.add_middleware(
     https_only=False,  # TODO: Set to True in production
 )
 app.add_middleware(
-    CSRFMiddleware,
+    CustomCSRFMiddleware,
     secret=SECRET_KEY,
+    exempt_urls=[re.compile(r"^/oauth/.*"), re.compile(r"^/ws")],
 )
 
+app.include_router(oauth.router)
 app.include_router(identity.router)
 app.include_router(platform.router)
 app.include_router(rom.router)
@@ -44,9 +48,11 @@ app.include_router(search.router)
 add_pagination(app)
 app.mount("/ws", socket_app)
 
+
 @app.get("/heartbeat")
 def heartbeat():
     return {"status": "ok"}
+
 
 @app.on_event("startup")
 def startup() -> None:
