ad76ed1dd0f5ddafa951c8bda6b37d5233693558
zurdi <zurdizurdo25@gmail.com>
2023-08-12 18:46:37 +0200

    drawer refactor to add logout button

---
+++ frontend/src/components/Drawer/LogoutBtn.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps(["rail"]);
+</script>
+
+<template>
+  <v-btn
+    class="drawer-item"
+    @click="logout()"
+    rounded="0"
+    color="primary"
+    block
+  >
+    {{ rail ? "" : "Logout" }}
+    <v-icon class="ml-3">mdi-logout-variant</v-icon>
+  </v-btn>
+</template>
