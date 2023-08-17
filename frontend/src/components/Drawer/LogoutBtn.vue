4f75ed013ad8e0cbd620fd08ef9dae817deaff09
zurdi <zurdizurdo25@gmail.com>
2023-08-17 18:26:40 +0200

    logout btn now shows logged user

--- frontend/src/components/Drawer/LogoutBtn.vue
+++ frontend/src/components/Drawer/LogoutBtn.vue
@@ -12,14 +12,16 @@ async function logout() {
 </script>
 
 <template>
-  <v-btn
-    class="drawer-item"
-    @click="logout()"
-    rounded="0"
-    color="primary"
-    block
-  >
-    {{ rail ? "" : "Logout" }}
-    <v-icon :class="{'ml-3': !rail}">mdi-logout-variant</v-icon>
-  </v-btn>
+  <v-list-item height="60" class="bg-primary text-button" rounded="0">
+    {{ rail ? "" : "Zurdi" }}
+    <template v-slot:prepend>
+      <v-avatar :class="{ 'ml-4': rail, 'my-3': rail }">
+        <v-img src="/assets/default_user.png" />
+      </v-avatar>
+    </template>
+    <template v-slot:append>
+      <v-btn v-if="!rail" variant="text" icon="mdi-location-exit" @click="logout()"></v-btn>
+    </template>
+  </v-list-item>
+  <v-btn v-if="rail" variant="text" icon="mdi-location-exit" class="ml-7 my-1" @click="logout()"></v-btn>
 </template>
