8c45c6bd0ae86e6028371c7677701559865f5d09
zurdi <zurdizurdo25@gmail.com>
2023-08-23 19:37:38 +0200

    little dashboard ui tweak

--- frontend/src/views/Dashboard/Summary.vue
+++ frontend/src/views/Dashboard/Summary.vue
@@ -8,11 +8,11 @@ const platforms = storePlatforms();
   <v-card rounded="0">
     <v-card-text class="py-0 px-2">
       <v-chip-group>
-        <v-chip class="text-overline bg-secondary" label>
+        <v-chip class="text-overline" variant="text" label>
           <v-icon class="mr-2">mdi-controller</v-icon
           >{{ platforms.value.length }} Platforms
         </v-chip>
-        <v-chip class="text-overline bg-secondary" label>
+        <v-chip class="text-overline" variant="text" label>
           <v-icon class="mr-2">mdi-disc</v-icon>{{ platforms.totalGames }} Games
         </v-chip>
       </v-chip-group>
