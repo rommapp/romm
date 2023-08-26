f26147e9cffcfaafbe8987c1514d399323b2807c
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-08-26 13:26:52 -0400

    Add script to init scheduler

--- backend/utils/redis.py
+++ backend/utils/redis.py
@@ -5,7 +5,7 @@ from config import REDIS_HOST, REDIS_PORT
 
 
 redis_client = Redis(host=REDIS_HOST, port=int(REDIS_PORT), db=0)
-redis_url = f"redis://{REDIS_HOST}:{REDIS_PORT}"
+redis_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
 
 high_prio_queue = Queue(name="high", connection=redis_client)
 default_queue = Queue(name="default", connection=redis_client)
