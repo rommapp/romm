07b89045cf01b3554e4d470b412c87921d19708f
zurdi <zurdizurdo25@gmail.com>
2023-08-29 00:01:31 +0200

    edit rom fixed from merge

--- frontend/src/components/Dialog/Rom/EditRom.vue
+++ frontend/src/components/Dialog/Rom/EditRom.vue
@@ -19,6 +19,10 @@ emitter.on("showEditRomDialog", (romToEdit) => {
 
 async function updateRom() {
   if (rom.value.file_name.includes("/")) {
+    emitter.emit("snackbarShow", {
+      msg: "Couldn't edit rom: invalid file name characters",
+      icon: "mdi-close-circle",
+      color: "red",
     });
     return;
   } else if (!rom.value.file_name) {
