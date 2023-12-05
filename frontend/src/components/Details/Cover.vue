902bd5ba26adfc017f8e26f14b3304f99153f00c
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-12-05 13:50:51 -0500

    [ROMM-455] Fallback to screenshot when cover missing

--- frontend/src/components/Details/Cover.vue
+++ frontend/src/components/Details/Cover.vue
@@ -10,7 +10,7 @@ const props = defineProps(["rom"]);
     :loading="downloadStore.value.includes(rom.id) ? 'romm-accent-1' : null"
   >
     <v-img
-      :src="`/assets/romm/resources/${rom.path_cover_l}`"
+      :src="`/assets/romm/resources/${rom.path_cover_l || rom.path_screenshots[0]}`"
       :lazy-src="`/assets/romm/resources/${rom.path_cover_s}`"
       :aspect-ratio="3 / 4"
     >
