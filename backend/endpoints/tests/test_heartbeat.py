933f921536c205598ae0f692cb70de906e099272
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2023-12-28 14:10:20 -0500

    Fix tests

--- backend/endpoints/tests/test_heartbeat.py
+++ backend/endpoints/tests/test_heartbeat.py
@@ -11,17 +11,17 @@ def test_heartbeat():
     assert response.status_code == 200
     heartbeat = response.json()
     assert heartbeat.get('VERSION') == get_version()
-    assert heartbeat.get('ROMM_AUTH_ENABLED') == True
-    assert heartbeat.get('WATCHER').get('ENABLED') == True
+    assert heartbeat.get('ROMM_AUTH_ENABLED')
+    assert heartbeat.get('WATCHER').get('ENABLED')
     assert heartbeat.get('WATCHER').get('TITLE') == "Rescan on filesystem change"
-    assert heartbeat.get('SCHEDULER').get('RESCAN').get('ENABLED') == True
+    assert heartbeat.get('SCHEDULER').get('RESCAN').get('ENABLED')
     assert heartbeat.get('SCHEDULER').get('RESCAN').get('CRON') == "0 3 * * *"
     assert heartbeat.get('SCHEDULER').get('RESCAN').get('TITLE') == "Scheduled rescan"
-    assert heartbeat.get('SCHEDULER').get('SWITCH_TITLEDB').get('ENABLED') == True
+    assert heartbeat.get('SCHEDULER').get('SWITCH_TITLEDB').get('ENABLED')
     assert heartbeat.get('SCHEDULER').get('SWITCH_TITLEDB').get('CRON') == "0 4 * * *"
     assert heartbeat.get('SCHEDULER').get('SWITCH_TITLEDB').get('TITLE') == "Scheduled Switch TitleDB update"
-    assert heartbeat.get('SCHEDULER').get('MAME_XML').get('ENABLED') == True
+    assert heartbeat.get('SCHEDULER').get('MAME_XML').get('ENABLED')
     assert heartbeat.get('SCHEDULER').get('MAME_XML').get('CRON') == "0 5 * * *"
     assert heartbeat.get('SCHEDULER').get('MAME_XML').get('TITLE') == "Scheduled MAME XML update"
-    assert heartbeat.get('CONFIG').get('EXCLUDED_MULTI_FILES') == []
-    assert heartbeat.get('CONFIG').get('EXCLUDED_SINGLE_EXT') == []
+    assert heartbeat.get('CONFIG').get('EXCLUDED_MULTI_FILES') is None
+    assert heartbeat.get('CONFIG').get('EXCLUDED_SINGLE_EXT') is None
