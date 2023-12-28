2cb910d7c65abf4ee1c6b83c528c41aa9ff0de69
Zurdi <zurdizurdo25@gmail.com>
2023-12-28 15:27:20 +0100

    some UI tweaks

--- frontend/src/components/Settings/Config/PlatformBindingCard.vue
+++ frontend/src/components/Settings/Config/PlatformBindingCard.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { ref, onBeforeMount, inject } from "vue";
+import { useDisplay } from "vuetify";
 import { api } from "@/services/api";
 import storeHeartbeat from "@/stores/heartbeat";
 import PlatformIcon from "@/components/Platform/PlatformIcon.vue";
 import CreatePlatformBindingDialog from "@/components/Dialog/Platform/CreatePlatformBinding.vue";
 
 // Props
+const { xs, sm, md, lgAndUp, name } = useDisplay();
 const emitter = inject("emitter");
 const heartbeat = storeHeartbeat();
 const platformsBinding = heartbeat.data.CONFIG.PLATFORMS_BINDING;
@@ -15,7 +17,7 @@ const platformsBinding = heartbeat.data.CONFIG.PLATFORMS_BINDING;
     <v-toolbar class="bg-terciary" density="compact">
       <v-toolbar-title class="text-button">
         <v-icon class="mr-3">mdi-controller</v-icon>
-        Platforms Binding
+        Platforms Bindings
       </v-toolbar-title>
       <v-btn
         disabled
@@ -24,7 +26,7 @@ const platformsBinding = heartbeat.data.CONFIG.PLATFORMS_BINDING;
         class="text-romm-accent-1"
         @click="emitter.emit('showCreatePlatformBindingDialog')"
       >
-        Add Binding
+        Add
       </v-btn>
     </v-toolbar>
 
@@ -32,15 +34,15 @@ const platformsBinding = heartbeat.data.CONFIG.PLATFORMS_BINDING;
 
     <v-card-text class="pa-1">
       <v-row no-gutters>
-        <v-col cols="2" v-for="(platform, fs_platform) in platformsBinding">
+        <v-col cols="6" sm="3" md="2" lg="2" v-for="(platform, fs_platform) in platformsBinding">
           <v-card-text class="bg-terciary ma-1 py-1 pl-1 pr-3">
             <v-row class="align-center" no-gutters>
-              <v-col cols="6">
-                <v-avatar class="mx-2" :rounded="0" size="60">
+              <v-col cols="5" sm="4" md="5" lg="4">
+                <v-avatar class="mx-2" :rounded="0" size="40">
                   <platform-icon :platform="platform" />
                 </v-avatar>
               </v-col>
-              <v-col cols="6">
+              <v-col cols="7" sm="8" md="7" lg="8">
                 <div class="bg-primary pa-2 text-caption text-truncate" :title="fs_platform">
                   <span clas="pa-1">{{ fs_platform }}</span>
                 </div>
