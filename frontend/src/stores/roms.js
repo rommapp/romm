fda0fd94f93687e3a3229852deba37b2cf00ca17
Georges-Antoine Assi <georges.antoine.assi@gmail.com>
2024-01-02 09:55:24 -0500

    Dont group roms with null IGDB ID

--- frontend/src/stores/roms.js
+++ frontend/src/stores/roms.js
@@ -1,5 +1,6 @@
-import { uniqBy, groupBy } from "lodash";
+import { uniqBy, groupBy, isNull } from "lodash";
 import { defineStore } from "pinia";
+import { nanoid } from "nanoid";
 
 export default defineStore("roms", {
   state: () => ({
@@ -32,7 +33,7 @@ export default defineStore("roms", {
       // Sort roms by comparator string
       this._all = this._all.sort((a, b) => {
         return a.sort_comparator.localeCompare(b.sort_comparator);
-      }),
+      });
       this._all = uniqBy(this._all, "id");
 
       // Check if roms should be grouped
@@ -43,7 +44,12 @@ export default defineStore("roms", {
       }
 
       // Group roms by igdb_id
-      this._grouped = Object.values(groupBy(this._all, "igdb_id")).map((games) => ({
+      this._grouped = Object.values(
+        groupBy(this._all, (game) =>
+          // If igdb_id is null, generate a random id so that the roms are not grouped
+          isNull(game.igdb_id) ? nanoid() : game.igdb_id
+        )
+      ).map((games) => ({
         ...games.shift(),
         siblings: games,
       }));
