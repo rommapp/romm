7a2ae7341debb67e3b7e9e076aee0729f5d1e57f
bfenty <bfenty@gmail.com>
2024-01-02 13:56:41 -0600

    added a maximum length to file extensions
    found that the code errors when there is a period in a filename (such as hidden files in UNIX OSes) and the file doesn't have a proper extension. Apple is really bad about putting these kinds of files in. To fix it, I added a new variable to the _exclude_files function, max_ext_length and set it to default to 4. That way if another function needs a different max it can override the default, but it shouldn't require other code changes.

--- backend/utils/fs.py
+++ backend/utils/fs.py
@@ -225,7 +225,7 @@ def get_roms_structure(fs_slug: str):
     )
 
 
-def _exclude_files(files, filetype) -> list[str]:
+def _exclude_files(files, filetype, max_ext_length=4) -> list[str]: # added a variable for maximum length of file extension, defaults to 4
     excluded_extensions = config[f"EXCLUDED_{filetype.upper()}_EXT"]
     excluded_names = config[f"EXCLUDED_{filetype.upper()}_FILES"]
     filtered_files: list = []
@@ -233,8 +233,8 @@ def _exclude_files(files, filetype) -> list[str]:
     for file in files:
         # Split the file name to get the extension.
         parts = file.split(".")
-        # Exclude the file if it has no extension or the extension is in the excluded list.
-        if len(parts) == 1 or parts[-1] in excluded_extensions:
+        # Exclude the file if it has no extension, has an extension longer than max_ext_length characters, or the extension is in the excluded list.
+        if len(parts) == 1 or len(parts[-1]) > max_ext_length or parts[-1] in excluded_extensions:
             filtered_files.append(file)
         # Additionally, check if the entire file name is in the excluded names list.
         elif file in excluded_names:
