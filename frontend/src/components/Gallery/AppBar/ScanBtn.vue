d4c0e254b959a8ef78f1fee797a46879c650c6f9
Zurdi <zurdizurdo25@gmail.com>
2024-01-16 18:13:12 +0100

    scan feedback is now more consistent

--- frontend/src/components/Gallery/AppBar/ScanBtn.vue
+++ frontend/src/components/Gallery/AppBar/ScanBtn.vue
@@ -1,77 +1,50 @@
 <script setup lang="ts">
-import type { Events } from "@/types/emitter";
-import type { Emitter } from "mitt";
-import { inject, onBeforeUnmount } from "vue";
-import { useRoute } from "vue-router";
-
 import socket from "@/services/socket";
 import storeGalleryFilter from "@/stores/galleryFilter";
 import storePlatforms from "@/stores/platforms";
 import storeRoms from "@/stores/roms";
 import storeScanning from "@/stores/scanning";
+import type { Events } from "@/types/emitter";
 import { normalizeString } from "@/utils";
+import type { Emitter } from "mitt";
+import { inject, onBeforeUnmount } from "vue";
+import { useRoute } from "vue-router";
 
 // Props
 const emitter = inject<Emitter<Events>>("emitter");
 const platformsStore = storePlatforms();
 const route = useRoute();
 const platform = platformsStore.get(Number(route.params.platform));
-const scanning = storeScanning();
+const scanningStore = storeScanning();
 const romsStore = storeRoms();
 const galleryFilter = storeGalleryFilter();
 const isFiltered = normalizeString(galleryFilter.filter).trim() != "";
 
-socket.on("scan:scanning_rom", (rom) => {
-  romsStore.add([rom]);
-  if (isFiltered) {
-    romsStore.setFiltered(romsStore.filteredRoms);
-  } else {
-    romsStore.setFiltered(romsStore.allRoms);
-  }
-});
-
-// Functions
-socket.on("scan:done", () => {
-  scanning.set(false);
-  socket.disconnect();
-  emitter?.emit("refreshDrawer", null);
-  emitter?.emit("snackbarShow", {
-    msg: "Scan completed successfully!",
-    icon: "mdi-check-bold",
-    color: "green",
-    timeout: 4000,
-  });
-});
-
-socket.on("scan:done_ko", (msg) => {
-  scanning.set(false);
-  emitter?.emit("snackbarShow", {
-    msg: `Scan couldn't be completed. Something went wrong: ${msg}`,
-    icon: "mdi-close-circle",
-    color: "red",
-  });
-  socket.disconnect();
-});
+// socket.on("scan:scanning_rom", ({ ...rom }) => {
+//   romsStore.add([rom]);
+//   if (isFiltered) {
+//     romsStore.setFiltered(romsStore.filteredRoms);
+//   } else {
+//     romsStore.setFiltered(romsStore.allRoms);
+//   }
+// });
 
 async function scan() {
-  scanning.set(true);
-  emitter?.emit("snackbarShow", {
-    msg: `Scanning ${platform?.name}...`,
-    icon: "mdi-loading mdi-spin",
-    color: "romm-accent-1",
-  });
+  scanningStore.set(true);
 
   if (!socket.connected) socket.connect();
+
   socket.emit("scan", {
     platforms: [platform?.id],
-    rescan: false,
+    completeRescan: false,
   });
 }
 
-onBeforeUnmount(() => {
-  socket.off("scan:done");
-  socket.off("scan:done_ko");
-});
+// onBeforeUnmount(() => {
+//   socket.off("scan:scanning_rom_s");
+//   socket.off("scan:done_s");
+//   socket.off("scan:done_ko_s");
+// });
 </script>
 
 <template>
--- frontend/src/stores/scanning.ts
+++ frontend/src/stores/scanning.ts
@@ -1,11 +1,20 @@
 import { defineStore } from "pinia";
+import type { Rom } from "@/stores/roms";
 
 export default defineStore("scanning", {
-  state: () => ({ value: false }),
+  state: () => ({
+    scanning: false,
+    scanningPlatforms: [] as {
+      name: string;
+      slug: string;
+      id: number;
+      roms: Rom[];
+    }[],
+  }),
 
   actions: {
     set(scanning: boolean) {
-      this.value = scanning;
+      this.scanning = scanning;
     },
   },
 });
