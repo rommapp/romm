871eedc6bc78f22eca4252f1a3f038e0fd1207fe
Zurdi <zurdizurdo25@gmail.com>
2024-01-16 18:27:16 +0100

    fixed user tab visibility

--- frontend/src/views/Settings/ControlPanel/Base.vue
+++ frontend/src/views/Settings/ControlPanel/Base.vue
@@ -30,7 +30,7 @@ const { scanning } = storeToRefs(scanningStore);
       <v-tab value="general" rounded="0">General</v-tab>
       <v-tab value="config" rounded="0">Config</v-tab>
       <v-tab
-        :disabled="!authStore.scopes.includes('users.read')"
+        v-if="heartbeatStore.value.ROMM_AUTH_ENABLED && authStore.scopes.includes('users.read')"
         value="users"
         rounded="0"
       >
