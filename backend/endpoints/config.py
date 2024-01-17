0beb6ace92be9974deb7bec2cfc483f66ac8bb23
Zurdi <zurdizurdo25@gmail.com>
2024-01-17 01:56:14 +0100

    exclusion endpoints added

--- backend/endpoints/config.py
+++ backend/endpoints/config.py
@@ -40,3 +40,22 @@ async def delete_platform_binding(request: Request) -> MessageResponse:
     cm.remove_binding(fs_slug)
 
     return {"msg": f"{fs_slug} bind removed successfully!"}
+
+
+@protected_route(router.post, "/config/exclude", ["platforms.write"])
+async def add_exclusion(request: Request) -> MessageResponse:
+    """Add platform binding to the configuration"""
+
+    data = await request.json()
+
+    return {"msg": f"Exclusion added successfully!"}
+
+
+@protected_route(router.delete, "/config/exclude", ["platforms.write"])
+async def delete_exclusion(request: Request) -> MessageResponse:
+    """Delete platform binding from the configuration"""
+
+    data = await request.json()
+
+    return {"msg": f"Exclusion removed successfully!"}
+
