7fbbc6ea3815d8e169a71400b2c5a08f2c14e3f1
Zurdi <zurdizurdo25@gmail.com>
2024-01-20 22:14:19 +0100

    added loading splash at play

--- frontend/src/components/Details/Emulation.vue
+++ frontend/src/components/Details/Emulation.vue
@@ -18,9 +18,7 @@ script.src = "/assets/emulatorjs/loader.js";
 script.async = true;
 
 function onPlay() {
-  // let backgroundImage = `/assets/romm/resources/${props.rom.platform_slug}/${props.rom.name}/screenshots/states/${stateRef.value?.file_name}.jpg`
-  let backgroundImage = `/assets/romm/resources/${props.rom.path_cover_l}`;
-  window.EJS_backgroundImage = backgroundImage
+  window.EJS_backgroundImage = "/assets/emulatorjs/loading_black.png"
   window.EJS_fullscreenOnLoaded = fullScreenOnPlay.value;
   document.body.appendChild(script);
   gameRunning.value = true;
@@ -107,7 +105,7 @@ onMounted(() => {
   </v-row>
 
   <v-row no-gutters class="align-center mt-6" v-if="!gameRunning">
-    <v-col cols="12" lg="7" xl="7">
+    <v-col cols="5">
       <v-btn
         block
         density="compact"
@@ -119,11 +117,11 @@ onMounted(() => {
         <v-icon class="mr-2">mdi-play</v-icon>Play
       </v-btn>
     </v-col>
-    <v-col cols="12" lg="5" xl="5">
-      <img
-        :class="{ 'mt-6': smAndDown, 'ml-7': mdAndUp }"
+    <v-spacer />
+    <v-col cols="5">
+      <imgº
         width="150"
-        src="/assets/powered_by_emulatorjs.png"
+        src="/assets/emulatorjs/powered_by_emulatorjs.png"
       />
     </v-col>
   </v-row>
