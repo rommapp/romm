fa23be9b363f2c542bf3ad6494dceb12db3d5745
Zurdi <zurdizurdo25@gmail.com>
2024-01-31 11:23:50 +0100

    added dynamic show for sort and filter bar

---
+++ frontend/src/components/Gallery/AppBar/FilterBtn.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { Events } from "@/types/emitter";
+import type { Emitter } from "mitt";
+import { ref, inject } from "vue";
+
+// Props
+const emitter = inject<Emitter<Events>>("emitter");
+const isShowFilterBar = ref(false);
+function showFilterBar() {
+  emitter?.emit('filterBarShow', null)
+  isShowFilterBar.value = !isShowFilterBar.value
+}
+</script>
+
+<template>
+  <v-btn
+    class="ml-0"
+    variant="text"
+    rounded="0"
+    icon="mdi-filter-variant"
+    :color="isShowFilterBar ? 'romm-accent-1' : ''"
+    title="Filter gallery"
+    @click="showFilterBar"
+  />
+</template>
---
+++ frontend/src/components/Gallery/AppBar/FilterTextField.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import type { Events } from "@/types/emitter";
+import { debounce } from "lodash";
+import type { Emitter } from "mitt";
+import { inject, onMounted, ref } from "vue";
+
+import storeGalleryFilter from "@/stores/galleryFilter";
+
+// Props
+const galleryFilter = storeGalleryFilter();
+const filterValue = ref("");
+
+// Event listeners bus
+const emitter = inject<Emitter<Events>>("emitter");
+
+function clearFilter() {
+  galleryFilter.set("");
+  emitter?.emit("filter", null);
+}
+
+const filterRoms = debounce(() => {
+  galleryFilter.set(filterValue.value);
+  emitter?.emit("filter", null);
+}, 500);
+
+onMounted(() => {
+  filterValue.value = galleryFilter.filter;
+});
+</script>
+
+<template>
+  <v-text-field
+    @click:clear="clearFilter"
+    @keyup="filterRoms"
+    v-model="filterValue"
+    label="Filter"
+    rounded="0"
+    hide-details
+    clearable
+  />
+</template>
