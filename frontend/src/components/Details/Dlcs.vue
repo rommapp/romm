1fb6003e9f4e5731da96584e37a8bac4b47e1e9d
Zurdi <zurdizurdo25@gmail.com>
2024-02-03 11:57:45 +0100

    added action bar to extra covers

--- frontend/src/components/Details/Dlcs.vue
+++ frontend/src/components/Details/Dlcs.vue
@@ -1,19 +1,25 @@
 <script setup lang="ts">
 import type { Rom } from "@/stores/roms";
+import ActionBar from "../Game/Card/ActionBar.vue";
 
 defineProps<{ rom: Rom }>();
 </script>
 <template>
-  <v-row class="mb-3" no-gutters v-for="dlc in rom.dlcs">
-    <v-img
-      class="cover"
-      :src="`https:${dlc.cover.url.replace('t_thumb', 't_cover_big')}`"
-      :lazy-src="`https:${dlc.cover.url.replace('t_thumb', 't_cover_small')}`"
-      :aspect-ratio="3 / 4"
-    ></v-img>
-    <v-col class="ml-3">
-      <span>{{ dlc.name }}</span>
-    </v-col>
+  <v-row class="mb-3" no-gutters >
+    <v-col cols="3" v-for="dlc in rom.dlcs">
+      <v-card class="ma-2">
+      <v-img
+        class="cover"
+        :src="`https:${dlc.cover.url.replace('t_thumb', 't_cover_big')}`"
+        :lazy-src="`https:${dlc.cover.url.replace('t_thumb', 't_cover_small')}`"
+        :aspect-ratio="3 / 4"
+      />
+      <action-bar :rom="rom" />
+    </v-card>
+    <!-- <v-col class="ml-3">
+      <span class="text-caption">{{ dlc.name }}</span>
+    </v-col> -->
+  </v-col>
   </v-row>
 </template>
 
--- frontend/src/components/Details/Expansions.vue
+++ frontend/src/components/Details/Expansions.vue
@@ -1,22 +1,30 @@
 <script setup lang="ts">
 import type { Rom } from "@/stores/roms";
+import ActionBar from "../Game/Card/ActionBar.vue";
 
 defineProps<{ rom: Rom }>();
 </script>
 <template>
-  <v-row class="mb-3" no-gutters v-for="expansion in rom.expansions">
-    <v-img
-      class="cover"
-      :src="`https:${expansion.cover.url.replace('t_thumb', 't_cover_big')}`"
-      :lazy-src="`https:${expansion.cover.url.replace(
-        't_thumb',
-        't_cover_small'
-      )}`"
-      :aspect-ratio="3 / 4"
-    ></v-img>
-    <v-col class="ml-3">
-      <span>{{ expansion.name }}</span>
+  <v-row class="mb-3" no-gutters >
+    <v-col cols="3" lg="4" v-for="expansion in rom.expansions">
+    
+      <v-card class="ma-3">
+  
+        <v-img
+          class="cover"
+          :src="`https:${expansion.cover.url.replace('t_thumb', 't_cover_big')}`"
+          :lazy-src="`https:${expansion.cover.url.replace(
+            't_thumb',
+            't_cover_small'
+          )}`"
+          :aspect-ratio="3 / 4"
+        />
+        <action-bar :rom="rom" />
+      </v-card>
     </v-col>
+    <!-- <v-col class="ml-3">
+      <span class="text-caption">{{ expansion.name }}</span>
+    </v-col> -->
   </v-row>
 </template>
 
