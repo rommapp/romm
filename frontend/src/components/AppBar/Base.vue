7ee7344a2d123a1af811af7eb7174d864264afcf
Zurdi <zurdizurdo25@gmail.com>
2024-02-06 22:38:12 +0100

    added version to login page

--- frontend/src/components/AppBar/Base.vue
+++ frontend/src/components/AppBar/Base.vue
@@ -1,21 +1,9 @@
 <script setup lang="ts">
 import DrawerToggle from "@/components/AppBar/DrawerToggle.vue";
-import storeScanning from "@/stores/scanning";
-import { storeToRefs } from "pinia";
-
-// Props
-const scanningStore = storeScanning();
-const { scanning } = storeToRefs(scanningStore);
 </script>
 
 <template>
   <v-app-bar elevation="1" density="comfortable">
-    <v-progress-linear
-      color="romm-accent-1"
-      :active="scanning"
-      :indeterminate="true"
-      absolute
-    />
     <drawer-toggle class="ml-5" />
   </v-app-bar>
 </template>
