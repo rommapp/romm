1fe7d3589a52b7e8b5c51474d8e545b7a751dd91
Georges-Antoine Assi <contact@me.gantoine.com>
2024-03-26 18:05:58 -0400

    autoload indexes in memory

--- backend/handler/metadata_handler/__init__.py
+++ backend/handler/metadata_handler/__init__.py
@@ -12,27 +12,38 @@ from tasks.update_switch_titledb import (
     SWITCH_PRODUCT_ID_KEY,
 )
 
+# These are loaded in cache in update_switch_titledb_task
+SWITCH_TITLEDB_REGEX: Final = r"(70[0-9]{12})"
+SWITCH_PRODUCT_ID_REGEX: Final = r"(0100[0-9A-F]{12})"
 
+# No regex needed for MAME
+MAME_XML_FILE: Final = os.path.join(os.path.dirname(__file__), "fixtures", "mame.xml")
+mame_xml_data = xmltodict.parse(open(MAME_XML_FILE, "r").read())
+
+# PS2 OPL
 PS2_OPL_REGEX: Final = r"^([A-Z]{4}_\d{3}\.\d{2})\..*$"
 PS2_OPL_INDEX_FILE: Final = os.path.join(
     os.path.dirname(__file__), "fixtures", "ps2_opl_index.json"
 )
+ps2_opl_index_data = json.loads(open(PS2_OPL_INDEX_FILE, "r").read())
 
-SWITCH_TITLEDB_REGEX: Final = r"(70[0-9]{12})"
-SWITCH_PRODUCT_ID_REGEX: Final = r"(0100[0-9A-F]{12})"
-
-MAME_XML_FILE: Final = os.path.join(os.path.dirname(__file__), "fixtures", "mame.xml")
-
+# Sony serial codes for PS1, PS2, and PSP
 SONY_SERIAL_REGEX: Final = r".*([a-zA-Z]{4}-\d{5}).*$"
+
 PS1_SERIAL_INDEX_FILE: Final = os.path.join(
     os.path.dirname(__file__), "fixtures", "ps1_serial_index.json"
 )
+ps1_serial_index_data = json.loads(open(PS1_SERIAL_INDEX_FILE, "r").read())
+
 PS2_SERIAL_INDEX_FILE: Final = os.path.join(
     os.path.dirname(__file__), "fixtures", "ps2_serial_index.json"
 )
+ps2_serial_index_data = json.loads(open(PS2_SERIAL_INDEX_FILE, "r").read())
+
 PSP_SERIAL_INDEX_FILE: Final = os.path.join(
     os.path.dirname(__file__), "fixtures", "psp_serial_index.json"
 )
+psp_serial_index_data = json.loads(open(PSP_SERIAL_INDEX_FILE, "r").read())
 
 
 class MetadataHandler:
@@ -83,44 +94,39 @@ class MetadataHandler:
 
     async def _ps2_opl_format(self, match: re.Match[str], search_term: str) -> str:
         serial_code = match.group(1)
-
-        with open(PS2_OPL_INDEX_FILE, "r") as index_json:
-            opl_index = json.loads(index_json.read())
-            index_entry = opl_index.get(serial_code, None)
-            if index_entry:
-                search_term = index_entry["Name"]  # type: ignore
+        index_entry = ps2_opl_index_data.get(serial_code, None)
+        if index_entry:
+            search_term = index_entry["Name"]  # type: ignore
 
         return search_term
 
     async def _sony_serial_format(
-        self, index_file: str, serial_code: str
+        self, index_data: dict, serial_code: str
     ) -> str | None:
-        with open(index_file, "r") as index_json:
-            opl_index = json.loads(index_json.read())
-            index_entry = opl_index.get(serial_code.upper(), None)
-            if index_entry:
-                return index_entry["title"]
+        index_entry = index_data.get(serial_code.upper(), None)
+        if index_entry:
+            return index_entry["title"]
 
         return None
 
     async def _ps1_serial_format(self, match: re.Match[str], search_term: str) -> str:
         serial_code = match.group(1)
         return (
-            await self._sony_serial_format(PS1_SERIAL_INDEX_FILE, serial_code)
+            await self._sony_serial_format(ps1_serial_index_data, serial_code)
             or search_term
         )
 
     async def _ps2_serial_format(self, match: re.Match[str], search_term: str) -> str:
         serial_code = match.group(1)
         return (
-            await self._sony_serial_format(PS2_SERIAL_INDEX_FILE, serial_code)
+            await self._sony_serial_format(ps2_serial_index_data, serial_code)
             or search_term
         )
 
     async def _psp_serial_format(self, match: re.Match[str], search_term: str) -> str:
         serial_code = match.group(1)
         return (
-            await self._sony_serial_format(PSP_SERIAL_INDEX_FILE, serial_code)
+            await self._sony_serial_format(psp_serial_index_data, serial_code)
             or search_term
         )
 
@@ -136,13 +142,13 @@ class MetadataHandler:
             if not cache.exists(SWITCH_TITLEDB_INDEX_KEY):
                 log.error("Could not fetch the Switch titleID index file")
                 return search_term, None
-        
-        index_entry = cache.hmget(SWITCH_TITLEDB_INDEX_KEY, title_id)
+
+        switch_index = json.loads(cache.get(SWITCH_TITLEDB_INDEX_KEY))
+        index_entry = switch_index.get(title_id, None)
         if index_entry:
             return index_entry["name"], index_entry  # type: ignore
-        
+
         return search_term, None
-    
 
     async def _switch_productid_format(
         self, match: re.Match[str], search_term: str
@@ -161,26 +167,25 @@ class MetadataHandler:
             if not cache.exists(SWITCH_PRODUCT_ID_KEY):
                 log.error("Could not fetch the Switch productID index file")
                 return search_term, None
-        
-        index_entry = cache.hmget(SWITCH_PRODUCT_ID_KEY, product_id)
+
+        switch_index = json.loads(cache.get(SWITCH_PRODUCT_ID_KEY))
+        index_entry = switch_index.get(product_id, None)
         if index_entry:
             return index_entry["name"], index_entry  # type: ignore
-        
+
         return search_term, None
 
     async def _mame_format(self, search_term: str) -> str:
         from handler import fs_rom_handler
 
-        with open(MAME_XML_FILE, "r") as index_xml:
-            mame_index = xmltodict.parse(index_xml.read())
-            index_entry = [
-                game
-                for game in mame_index["menu"]["game"]
-                if game["@name"] == search_term
-            ]
-            if index_entry:
-                search_term = fs_rom_handler.get_file_name_with_no_tags(
-                    index_entry[0].get("description", search_term)
-                )
+        index_entry = [
+            game
+            for game in mame_xml_data["menu"]["game"]
+            if game["@name"] == search_term
+        ]
+        if index_entry:
+            search_term = fs_rom_handler.get_file_name_with_no_tags(
+                index_entry[0].get("description", search_term)
+            )
 
         return search_term
