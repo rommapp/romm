3c686e6f0ad9e96971917092e2b2dcdc6c6f37cc
Georges-Antoine Assi <contact@me.gantoine.com>
2024-05-18 18:50:23 -0400

    add network quiest listener

--- frontend/src/services/api/index.ts
+++ frontend/src/services/api/index.ts
@@ -1,18 +1,36 @@
 import axios from "axios";
 import cookie from "js-cookie";
-
 import router from "@/plugins/router";
+import { debounce } from "lodash";
 
 const api = axios.create({ baseURL: "/api", timeout: 120000 });
 
+const inflightRequests = new Set();
+
+const networkQuiesced = debounce(() => {
+  document.dispatchEvent(new CustomEvent("network-quiesced"));
+}, 300);
+
 // Set CSRF header for all requests
 api.interceptors.request.use((config) => {
+  inflightRequests.add(config.url);
+  networkQuiesced.cancel();
+
   config.headers["x-csrftoken"] = cookie.get("csrftoken");
   return config;
 });
 
 api.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    inflightRequests.delete(response.config.url);
+
+    // If there are no more inflight requests, fetch home data
+    if (inflightRequests.size === 0) {
+      networkQuiesced();
+    }
+
+    return response;
+  },
   (error) => {
     if (error.response?.status === 403) {
       router.push({
