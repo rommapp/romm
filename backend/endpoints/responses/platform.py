6c726f91ac521161ff4718c7b92d32a2a2eb97cc
Georges-Antoine Assi <contact@me.gantoine.com>
2024-05-24 09:56:41 -0400

    hotfix scanning rom_count field

--- backend/endpoints/responses/platform.py
+++ backend/endpoints/responses/platform.py
@@ -8,12 +8,12 @@ class PlatformSchema(BaseModel):
     id: int
     slug: str
     fs_slug: str
+    name: str
+    rom_count: int
     igdb_id: Optional[int] = None
     sgdb_id: Optional[int] = None
     moby_id: Optional[int] = None
-    name: str
     logo_path: Optional[str] = ""
-    rom_count: int
     firmware: list[FirmwareSchema] = Field(default_factory=list)
 
     class Config:
