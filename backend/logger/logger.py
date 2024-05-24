95b525ecda99b8811484ea1b4dab36a6640722f0
Georges-Antoine Assi <contact@me.gantoine.com>
2024-05-24 12:01:47 -0400

    use platforms store + fix scan

--- backend/logger/logger.py
+++ backend/logger/logger.py
@@ -8,14 +8,14 @@ log = logging.getLogger("romm")
 log.setLevel(logging.DEBUG)
 
 # Set up sqlachemy logger
-sql_log = logging.getLogger("sqlalchemy.engine")
-sql_log.setLevel(logging.DEBUG)
+# sql_log = logging.getLogger("sqlalchemy.engine")
+# sql_log.setLevel(logging.DEBUG)
 
 # Define stdout handler
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(StdoutFormatter())
 log.addHandler(stdout_handler)
-sql_log.addHandler(stdout_handler)
+# sql_log.addHandler(stdout_handler)
 
 # Hush passlib warnings
 logging.getLogger("passlib").setLevel(logging.ERROR)
