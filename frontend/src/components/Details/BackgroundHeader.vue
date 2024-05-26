c570359dd0cbfbc345ee39f2a716e770b3812955
Georges-Antoine Assi <contact@me.gantoine.com>
2024-05-26 16:55:10 -0400

    use regular image tags

--- frontend/src/components/Details/BackgroundHeader.vue
+++ frontend/src/components/Details/BackgroundHeader.vue
@@ -10,17 +10,11 @@ const imgSrc =
     : !props.rom.has_cover
     ? `/assets/default/cover/big_${theme.global.name.value}_missing_cover.png`
     : `/assets/romm/resources/${props.rom.path_cover_s}`;
-const imgSrcLazy =
-  !props.rom.igdb_id && !props.rom.moby_id && !props.rom.has_cover
-    ? `/assets/default/cover/small_${theme.global.name.value}_unmatched.png`
-    : !props.rom.has_cover
-    ? `/assets/default/cover/small_${theme.global.name.value}_missing_cover.png`
-    : `/assets/romm/resources/${props.rom.path_cover_s}`;
 </script>
 
 <template>
   <v-card rounded="0">
-    <v-img :src="imgSrc" :lazy-src="imgSrcLazy" id="background-header" />
+    <v-img :src="imgSrc" id="background-header" />
   </v-card>
 </template>
 <style scoped>
