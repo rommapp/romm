37fe7870d3ec36480419c99ce20ccfe97a8d32bc
Zurdi <zurdizurdo25@gmail.com>
2024-05-31 18:07:48 +0200

    rommapp migration image

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,145 +1,54 @@
-<script setup lang="ts">
-import Notification from "@/components/Notification.vue";
-import api from "@/services/api/index";
-import userApi from "@/services/api/user";
-import platformApi from "@/services/api/platform";
-import socket from "@/services/socket";
-import storeConfig from "@/stores/config";
-import storeGalleryFilter from "@/stores/galleryFilter";
-import storeHeartbeat from "@/stores/heartbeat";
-import storeRoms, { type SimpleRom } from "@/stores/roms";
-import storePlatforms from "@/stores/platforms";
-import storeAuth from "@/stores/auth";
-import storeScanning from "@/stores/scanning";
-import type { Events } from "@/types/emitter";
-import { normalizeString } from "@/utils";
-import type { Emitter } from "mitt";
-import { storeToRefs } from "pinia";
-import { inject, onMounted, onBeforeUnmount } from "vue";
-
-// Props
-const scanningStore = storeScanning();
-const { scanningPlatforms } = storeToRefs(scanningStore);
-const romsStore = storeRoms();
-const galleryFilter = storeGalleryFilter();
-const isFiltered = normalizeString(galleryFilter.filterSearch).trim() != "";
-const emitter = inject<Emitter<Events>>("emitter");
-
-// Props
-const heartbeat = storeHeartbeat();
-const configStore = storeConfig();
-const auth = storeAuth();
-const platformsStore = storePlatforms();
-
-socket.on(
-  "scan:scanning_platform",
-  ({ name, slug, id }: { name: string; slug: string; id: number }) => {
-    scanningStore.set(true);
-    scanningPlatforms.value = scanningPlatforms.value.filter(
-      (platform) => platform.name !== name
-    );
-    scanningPlatforms.value.push({ name, slug, id, roms: [] });
-  }
-);
-
-socket.on("scan:scanning_rom", (rom: SimpleRom) => {
-  scanningStore.set(true);
-  if (romsStore.platformID === rom.platform_id) {
-    romsStore.add([rom]);
-    romsStore.setFiltered(
-      isFiltered ? romsStore.filteredRoms : romsStore.allRoms,
-      galleryFilter
-    );
-  }
-
-  let scannedPlatform = scanningPlatforms.value.find(
-    (p) => p.slug === rom.platform_slug
-  );
-
-  // Add the platform if the socket dropped and it's missing
-  if (!scannedPlatform) {
-    scanningPlatforms.value.push({
-      name: rom.platform_name,
-      slug: rom.platform_slug,
-      id: rom.platform_id,
-      roms: [],
-    });
-    scannedPlatform = scanningPlatforms.value[0];
-  }
-
-  scannedPlatform?.roms.push(rom);
-});
-
-socket.on("scan:done", () => {
-  scanningStore.set(false);
-  socket.disconnect();
-
-  emitter?.emit("refreshDrawer", null);
-  emitter?.emit("snackbarShow", {
-    msg: "Scan completed successfully!",
-    icon: "mdi-check-bold",
-    color: "green",
-    timeout: 4000,
-  });
-});
-
-socket.on("scan:done_ko", (msg) => {
-  scanningStore.set(false);
-
-  emitter?.emit("snackbarShow", {
-    msg: `Scan failed: ${msg}`,
-    icon: "mdi-close-circle",
-    color: "red",
-  });
-  socket.disconnect();
-});
-
-onBeforeUnmount(() => {
-  socket.off("scan:scanning_platform");
-  socket.off("scan:scanning_rom");
-  socket.off("scan:done");
-  socket.off("scan:done_ko");
-});
-
-onMounted(() => {
-  api.get("/heartbeat").then(({ data: data }) => {
-    heartbeat.set(data);
-  });
-
-  api.get("/config").then(({ data: data }) => {
-    configStore.set(data);
-  });
-
-  userApi
-    .fetchCurrentUser()
-    .then(({ data: user }) => {
-      auth.setUser(user);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-
-  platformApi
-    .getPlatforms()
-    .then(({ data: platforms }) => {
-      platformsStore.set(platforms);
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-});
-</script>
-
 <template>
   <v-app>
     <v-main>
-      <notification class="mt-6" />
-      <router-view />
+      <span id="bg"></span>
+
+      <v-container class="fill-height justify-center">
+        <v-card id="card" class="py-8 px-5" width="800">
+          <v-row>
+            <v-col>
+              <v-img
+                src="/assets/isotipo.svg"
+                class="mx-auto"
+                width="200"
+                height="200"
+              />
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-empty-state headline="RomM has a new house!"
+                ><h3>RomM has been migrated to the rommapp organization.</h3>
+                <p>
+                  Please switch to
+                  <a
+                    href="https://hub.docker.com/repository/docker/rommapp/romm"
+                    >rommapp/romm</a
+                  >, this repository will be no longer maintained.
+                </p></v-empty-state
+              >
+            </v-col>
+          </v-row>
+        </v-card>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 <style>
 body {
   background-color: rgba(var(--v-theme-background));
+}
+#bg {
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  background: url("/assets/login_bg.png") center center;
+  background-size: cover;
+}
+#card {
+  background: rgba(0, 0, 0, 0.35);
+  backdrop-filter: blur(10px);
 }
 </style>
--- frontend/src/plugins/router.ts
+++ frontend/src/plugins/router.ts
@@ -3,50 +3,9 @@ import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: "/login",
-    name: "login",
-    component: () => import("@/views/Login.vue"),
-  },
-  {
     path: "/",
     name: "home",
-    component: () => import("@/views/Home.vue"),
-    children: [
-      {
-        path: "/",
-        name: "dashboard",
-        component: () => import("@/views/Dashboard/Base.vue"),
-      },
-      {
-        path: "/platform/:platform",
-        name: "platform",
-        component: () => import("@/views/Gallery/Base.vue"),
-      },
-      {
-        path: "/rom/:rom",
-        name: "rom",
-        component: () => import("@/views/Details/Base.vue"),
-      },
-      {
-        path: "/library/scan",
-        name: "scan",
-        component: () => import("@/views/Library/Scan/Base.vue"),
-      },
-      {
-        path: "/settings/control-panel/",
-        name: "controlPanel",
-        component: () => import("@/views/Settings/ControlPanel/Base.vue"),
-      },
-      {
-        path: "/:pathMatch(.*)*",
-        name: "noMatch",
-        component: () => import("@/views/Dashboard/Base.vue"),
-      },
-    ],
-  },
-  {
-    path: "/play/:rom",
-    component: () => import("@/views/Play/Base.vue"),
+    component: () => import("@/App.vue"),
   },
 ];
 
