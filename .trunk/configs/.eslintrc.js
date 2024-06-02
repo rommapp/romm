f6c7aa4b09bbddd09bf6a646464f0529e43d5489
Georges-Antoine Assi <contact@me.gantoine.com>
2024-06-02 15:29:52 -0400

    playing around with trunk

--- .trunk/configs/.eslintrc.js
+++ .trunk/configs/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = tseslint.config(
   {
     ignores: ["node_modules", "dist", "__generated__", "*.config.js"],
     languageOptions: {
+      parser: "vue-eslint-parser",
       parserOptions: {
         parser: "@typescript-eslint/parser",
         project: "./tsconfig.json",
