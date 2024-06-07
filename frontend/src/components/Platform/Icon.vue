caf8b8fe07da951a9123088cf9798ce765bf84bf
zurdi <zurdizurdo25@gmail.com>
2024-06-07 09:17:54 +0200

    added romm-915

--- frontend/src/components/Platform/Icon.vue
+++ frontend/src/components/Platform/Icon.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import storeConfig from "@/stores/config";
+import { ref } from "vue";
+
 const props = defineProps<{ slug: string }>();
+const configStore = storeConfig();
+const slug = configStore.value.PLATFORMS_VERSIONS[props.slug] ?? props.slug;
+const platformIconUrl = ref(`/assets/platforms/${slug.toLowerCase()}.ico`);
 </script>
 
 <template>
-  <v-img :src="`/assets/platforms/${props.slug.toLowerCase()}.ico`"
+  <v-img :src="platformIconUrl"
     ><template #error
       ><v-img src="/assets/platforms/default.ico"></v-img></template
   ></v-img>
