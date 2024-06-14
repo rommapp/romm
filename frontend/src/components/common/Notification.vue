2136c2c967c6b67811d696485816c13dd28bb208
Zurdi <34356590+zurdi15@users.noreply.github.com>
2024-06-15 00:00:30 +0200

    Update frontend/src/components/common/Notification.vue
    Co-authored-by: Georges-Antoine Assi <3247106+gantoine@users.noreply.github.com>

--- frontend/src/components/common/Notification.vue
+++ frontend/src/components/common/Notification.vue
@@ -30,7 +30,7 @@ function closeDialog() {
   <v-snackbar
     transition="scroll-y-transition"
     v-model="show"
-    :timeout="snackbarStatus.timeout ? snackbarStatus.timeout : 3000"
+    :timeout="snackbarStatus.timeout || 3000"
     @timeout="closeDialog"
     absolute
     :location="xs ? 'top' : 'top right'"
