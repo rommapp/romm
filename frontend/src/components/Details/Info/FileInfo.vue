39877b13b7c9bfc8bfb5b3426fd3ebbdad23e1e5
zurdi <zurdizurdo25@gmail.com>
2024-06-27 23:20:37 +0200

    fixed game info details

--- frontend/src/components/Details/Info/FileInfo.vue
+++ frontend/src/components/Details/Info/FileInfo.vue
@@ -9,10 +9,12 @@ import { formatBytes } from "@/utils";
 import type { Emitter } from "mitt";
 import { inject } from "vue";
 
+// Props
 const props = defineProps<{ rom: DetailedRom; platform: Platform }>();
 const emitter = inject<Emitter<Events>>("emitter");
 const downloadStore = storeDownload();
 
+// Functions
 async function updateMainSibling() {
   const updatedRom = props.rom;
   updatedRom.fav_sibling = !props.rom.fav_sibling;
@@ -39,97 +41,107 @@ async function updateMainSibling() {
 }
 </script>
 <template>
-  <v-row
-    v-if="rom.sibling_roms && rom.sibling_roms.length > 0"
-    class="align-center my-3"
-    no-gutters
-  >
-    <v-col cols="3" xl="2">
-      <span>Version</span>
-    </v-col>
+  <v-row class="ml-4" no-gutters>
     <v-col>
-      <v-row class="align-center" no-gutters>
-        <version-switcher :rom="rom" :platform="platform" />
-        <v-tooltip
-          location="top"
-          class="tooltip"
-          transition="fade-transition"
-          text="Set as default version"
-          open-delay="300"
-        >
-          <template #activator="{ props }">
-            <v-chip
-              v-bind="props"
-              rounded="0"
-              variant="text"
-              class="ml-3"
-              @click="updateMainSibling"
-              ><v-icon
-                :class="rom.fav_sibling ? '' : 'mr-1'"
-                :color="rom.fav_sibling ? 'romm-accent-1' : ''"
-                >{{
-                  rom.fav_sibling
-                    ? "mdi-checkbox-outline"
-                    : "mdi-checkbox-blank-outline"
-                }}</v-icon
-              >{{ rom.fav_sibling ? "" : "Set as default" }}</v-chip
-            >
-          </template></v-tooltip
-        >
-      </v-row>
-    </v-col>
-  </v-row>
-  <v-row v-if="!rom.multi" class="align-center my-3" no-gutters>
-    <v-col cols="3" xl="2">
-      <span>File</span>
-    </v-col>
-    <v-col class="text-body-1">
-      <span>{{ rom.file_name }}</span>
-    </v-col>
-  </v-row>
-  <v-row v-if="rom.multi" class="align-center my-3" no-gutters>
-    <v-col cols="3" xl="2">
-      <span>Files</span>
-    </v-col>
-    <v-col>
-      <v-select
-        v-model="downloadStore.filesToDownloadMultiFileRom"
-        :label="rom.file_name"
-        item-title="file_name"
-        :items="rom.files"
-        class="my-2"
-        density="compact"
-        variant="outlined"
-        return-object
-        multiple
-        hide-details
-        clearable
-        chips
-      />
-    </v-col>
-  </v-row>
-  <v-row class="align-center my-3" no-gutters>
-    <v-col cols="3" xl="2">
-      <span>Size</span>
-    </v-col>
-    <v-col>
-      <span>{{ formatBytes(rom.file_size_bytes) }}</span>
-    </v-col>
-  </v-row>
-  <v-row v-if="rom.tags.length > 0" class="align-center my-3" no-gutters>
-    <v-col cols="3" xl="2">
-      <span>Tags</span>
-    </v-col>
-    <v-col>
-      <v-chip
-        v-for="tag in rom.tags"
-        :key="tag"
-        class="mr-2 py-1"
-        label
-        variant="outlined"
+      <v-row
+        v-if="rom.sibling_roms && rom.sibling_roms.length > 0"
+        class="align-center my-3"
+        no-gutters
       >
-        {{ tag }}
-      </v-chip>
+        <v-col cols="3" xl="2">
+          <span>Version</span>
+        </v-col>
+        <v-col>
+          <v-row class="align-center" no-gutters>
+            <version-switcher :rom="rom" :platform="platform" />
+            <v-tooltip
+              location="top"
+              class="tooltip"
+              transition="fade-transition"
+              text="Set as default version"
+              open-delay="300"
+            >
+              <template #activator="{ props }">
+                <v-btn
+                  v-bind="props"
+                  variant="flat"
+                  rounded="0"
+                  size="small"
+                  @click="updateMainSibling"
+                  ><v-icon
+                    :class="rom.fav_sibling ? '' : 'mr-1'"
+                    :color="rom.fav_sibling ? 'romm-accent-1' : ''"
+                    >{{
+                      rom.fav_sibling
+                        ? "mdi-checkbox-outline"
+                        : "mdi-checkbox-blank-outline"
+                    }}</v-icon
+                  >{{ rom.fav_sibling ? "" : "Default" }}</v-btn
+                >
+              </template></v-tooltip
+            >
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row v-if="!rom.multi" class="align-center my-3" no-gutters>
+        <v-col cols="3" xl="2">
+          <span>File</span>
+        </v-col>
+        <v-col>
+          <span class="text-body-1">{{ rom.file_name }}</span
+          ><v-chip class="ml-2" size="small" label>{{
+            formatBytes(rom.file_size_bytes)
+          }}</v-chip>
+        </v-col>
+      </v-row>
+      <v-row v-if="rom.multi" class="align-center my-3" no-gutters>
+        <v-col cols="3" xl="2">
+          <span>Files</span>
+        </v-col>
+        <v-col>
+          <v-row no-gutters class="align-center">
+            <v-col cols="9">
+              <v-select
+                v-model="downloadStore.filesToDownloadMultiFileRom"
+                :label="rom.file_name"
+                item-title="file_name"
+                :items="rom.files"
+                rounded="0"
+                density="compact"
+                variant="outlined"
+                return-object
+                multiple
+                hide-details
+                clearable
+                chips
+              />
+            </v-col>
+            <v-col>
+              <v-chip class="ml-2" size="small" label>{{
+                formatBytes(rom.file_size_bytes)
+              }}</v-chip>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row v-if="rom.tags.length > 0" class="align-center my-3" no-gutters>
+        <v-col cols="3" xl="2">
+          <span>Tags</span>
+        </v-col>
+        <v-col>
+          <v-chip
+            v-for="tag in rom.tags"
+            :key="tag"
+            size="small"
+            class="mr-2"
+            label
+            color="romm-accent-1"
+            variant="tonal"
+          >
+            {{ tag }}
+          </v-chip>
+        </v-col>
+      </v-row>
     </v-col>
   </v-row>
 </template>
