e1bcd9cc061234c662153e5a2a306a458898110e
Michael Manganiello <adamantike@users.noreply.github.com>
2024-06-30 12:19:37 -0300

    misc: Use Python future annotations
    Simplify annotations by using `__future__.annotations`, so there's no
    need to add typing using types as strings.

--- backend/utils/iterators.py
+++ backend/utils/iterators.py
@@ -3,9 +3,9 @@ import sys
 if sys.version_info >= (3, 12):
     from itertools import batched  # noqa: F401
 else:
-    from collections.abc import Iterator
+    from collections.abc import Iterable, Iterator
     from itertools import islice
-    from typing import Iterable, TypeVar
+    from typing import TypeVar
 
     T = TypeVar("T")
 
