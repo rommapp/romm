c5b150c1ebb872e0f1f9304a22b79eae169efea9
Michael Manganiello <adamantike@users.noreply.github.com>
2024-07-26 21:40:46 -0300

    fix: Enable flake8-async rules in ruff and fix warnings
    `ASYNC` rules [1] tries to find issues regarding asynchronous code. This
    change enables `ruff` to start reporting these issues, and fixes
    existing warnings.
    
    [1] https://docs.astral.sh/ruff/rules/#flake8-async-async

--- backend/endpoints/collections.py
+++ backend/endpoints/collections.py
@@ -1,6 +1,7 @@
 import json
 from shutil import rmtree
 
+from anyio import open_file
 from config import RESOURCES_BASE_PATH
 from decorators.auth import protected_route
 from endpoints.responses import MessageResponse
@@ -62,12 +63,12 @@ async def add_collection(
 
         artwork_file = artwork.file.read()
         file_location_s = f"{artwork_path}/small.{file_ext}"
-        with open(file_location_s, "wb+") as artwork_s:
+        async with await open_file(file_location_s, "wb+") as artwork_s:
             artwork_s.write(artwork_file)
             fs_resource_handler.resize_cover_to_small(file_location_s)
 
         file_location_l = f"{artwork_path}/big.{file_ext}"
-        with open(file_location_l, "wb+") as artwork_l:
+        async with await open_file(file_location_l, "wb+") as artwork_l:
             artwork_l.write(artwork_file)
     else:
         path_cover_s, path_cover_l = await fs_resource_handler.get_cover(
@@ -183,12 +184,12 @@ async def update_collection(
 
             artwork_file = artwork.file.read()
             file_location_s = f"{artwork_path}/small.{file_ext}"
-            with open(file_location_s, "wb+") as artwork_s:
+            async with await open_file(file_location_s, "wb+") as artwork_s:
                 artwork_s.write(artwork_file)
                 fs_resource_handler.resize_cover_to_small(file_location_s)
 
             file_location_l = f"{artwork_path}/big.{file_ext}"
-            with open(file_location_l, "wb+") as artwork_l:
+            async with await open_file(file_location_l, "wb+") as artwork_l:
                 artwork_l.write(artwork_file)
             cleaned_data.update({"url_cover": ""})
         else:
