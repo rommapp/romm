400cd50c5caa3900d43a943a8096b8ca65d6c3f5
Georges-Antoine Assi <contact@me.gantoine.com>
2024-07-27 16:38:57 -0400

    trunk fixes

--- backend/handler/tests/test_fastapi.py
+++ backend/handler/tests/test_fastapi.py
@@ -1,7 +1,7 @@
 import pytest
 from handler.scan_handler import ScanType, scan_platform, scan_rom
 from models.platform import Platform
-from models.rom import Rom
+from models.rom import Rom, RomFile
 from utils.context import initialize_context
 
 
@@ -31,12 +31,12 @@ async def test_scan_rom():
 
     async with initialize_context():
         files = [
-            {
-                "file_name": "Paper Mario (USA).z64",
-                "crc_hash": "9d0d1c6e",
-                "md5_hash": "f1b7f9e4f4d0e0b7b9faa1b1f2f8e4e9",
-                "sha1_hash": "c3c7f9f3d1d0e0b7b9faa1b1f2f8e4e9",
-            }
+            RomFile(
+                file_name="Paper Mario (USA).z64",
+                crc_hash="9d0d1c6e",
+                md5_hash="f1b7f9e4f4d0e0b7b9faa1b1f2f8e4e9",
+                sha1_hash="c3c7f9f3d1d0e0b7b9faa1b1f2f8e4e9",
+            )
         ]
 
         rom = await scan_rom(
