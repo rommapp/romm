8cd4cf1fe61f1091964776d0d1d213a32012d9fd
Georges-Antoine Assi <contact@me.gantoine.com>
2024-08-02 16:37:04 -0400

    pull sibling roms from backend

--- frontend/src/__generated__/models/Body_add_roms_roms_post.ts
+++ frontend/src/__generated__/models/Body_add_roms_roms_post.ts
@@ -1,6 +1,7 @@
 /* generated using openapi-typescript-codegen -- do no edit */
 /* istanbul ignore file */
 /* tslint:disable */
+/* eslint-disable */
 
 export type Body_add_roms_roms_post = {
   roms: Array<Blob>;
