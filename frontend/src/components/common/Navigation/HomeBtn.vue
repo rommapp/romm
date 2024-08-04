167c643798d4dcc83e41beec211213ed627a39d7
Zurdi <zurdizurdo25@gmail.com>
2024-08-05 00:24:44 +0200

    added right click to open in a new tab in home btn

--- frontend/src/components/common/Navigation/HomeBtn.vue
+++ frontend/src/components/common/Navigation/HomeBtn.vue
@@ -1,17 +1,21 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import RommIso from "@/components/common/RommIso.vue";
 import storeNavigation from "@/stores/navigation";
 
+const homeUrl = ref(`${location.protocol}//${location.host}`);
 const navigationStore = storeNavigation();
 </script>
 <template>
-  <v-hover v-slot="{ isHovering, props: hoverProps }">
-    <romm-iso
-      @click="navigationStore.goHome"
-      v-bind="hoverProps"
-      class="pointer"
-      :class="{ 'border-romm-accent-1': isHovering }"
-      :size="35"
-    />
-  </v-hover>
+  <a @click.prevent style="text-decoration: none; color: inherit" :href="homeUrl">
+    <v-hover v-slot="{ isHovering, props: hoverProps }">
+      <romm-iso
+        @click="navigationStore.goHome"
+        v-bind="hoverProps"
+        class="pointer"
+        :class="{ 'border-romm-accent-1': isHovering }"
+        :size="35"
+      />
+    </v-hover>
+  </a>
 </template>
