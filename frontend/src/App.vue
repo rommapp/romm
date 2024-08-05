443c307ea68e6011723ea2cc215aceaea8902409
Georges-Antoine Assi <contact@me.gantoine.com>
2024-08-05 13:13:25 -0400

    hopefully actually fix csrf tokens

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,50 +5,34 @@ import userApi from "@/services/api/user";
 import storeAuth from "@/stores/auth";
 import storeConfig from "@/stores/config";
 import storeHeartbeat from "@/stores/heartbeat";
-import storeNotifications from "@/stores/notifications";
-import Cookies from "js-cookie";
-import { storeToRefs } from "pinia";
 import { onBeforeMount } from "vue";
 import router from "./plugins/router";
 
 // Props
-const notificationStore = storeNotifications();
-const { notifications } = storeToRefs(notificationStore);
 const heartbeat = storeHeartbeat();
 const auth = storeAuth();
 const configStore = storeConfig();
 
-onBeforeMount(async () => {
-  await api
-    .get("/heartbeat")
-    .then(async ({ data: data }) => {
-      heartbeat.set(data);
-      if (heartbeat.value.SHOW_SETUP_WIZARD) {
-        router.push({ name: "setup" });
-      } else {
-        await userApi
-          .fetchCurrentUser()
-          .then(({ data: user }) => {
-            auth.setUser(user);
-          })
-          .catch((error) => {
-            console.error(error);
-          });
-
-        await api.get("/config").then(({ data: data }) => {
-          configStore.set(data);
+onBeforeMount(() => {
+  api.get("/heartbeat").then(async ({ data: data }) => {
+    heartbeat.set(data);
+    if (heartbeat.value.SHOW_SETUP_WIZARD) {
+      router.push({ name: "setup" });
+    } else {
+      await userApi
+        .fetchCurrentUser()
+        .then(({ data: user }) => {
+          auth.setUser(user);
+        })
+        .catch((error) => {
+          console.error(error);
         });
-      }
-    })
-    .catch(() => {
-      const allCookies = Cookies.get(); // Get all cookies
-      for (let cookie in allCookies) {
-        Cookies.remove(cookie); // Remove each cookie
-      }
-      router.push({
-        name: "login",
+
+      await api.get("/config").then(({ data: data }) => {
+        configStore.set(data);
       });
-    });
+    }
+  });
 });
 </script>
 
