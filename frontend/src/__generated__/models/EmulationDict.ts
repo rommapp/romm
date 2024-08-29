318be1840eb3659530179eaade6ee4757d093647
Georges-Antoine Assi <contact@me.gantoine.com>
2024-08-29 10:05:58 -0400

    [ROMM-1107] Add env variable to disable in-browser emulation

---
+++ frontend/src/__generated__/models/EmulationDict.ts
@@ -0,0 +1,10 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type EmulationDict = {
+    DISABLE_EMULATOR_JS: boolean;
+    DISABLE_RUFFLE_RS: boolean;
+};
+
