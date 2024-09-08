fa620fe4a8774debd18b07c74dc8067e9cc6ae6c
Georges-Antoine Assi <contact@me.gantoine.com>
2024-09-08 13:59:10 -0400

    add simple emojis to cards

--- frontend/src/components/common/Game/Card/Flags.vue
+++ frontend/src/components/common/Game/Card/Flags.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { computed } from "vue";
 import { type SimpleRom } from "@/stores/roms.js";
 import { languageToEmoji, regionToEmoji } from "@/utils";
+import type { RomUserStatus, RomUserSchema } from "@/__generated__";
 import { identity, isNull } from "lodash";
 
-defineProps<{ rom: SimpleRom }>();
+const props = defineProps<{ rom: SimpleRom }>();
 const showRegions = isNull(localStorage.getItem("settings.showRegions"))
   ? true
   : localStorage.getItem("settings.showRegions") === "true";
@@ -13,6 +15,34 @@ const showLanguages = isNull(localStorage.getItem("settings.showLanguages"))
 const showSiblings = isNull(localStorage.getItem("settings.showSiblings"))
   ? true
   : localStorage.getItem("settings.showSiblings") === "true";
+
+type PlayingStatus = RomUserStatus | "backlogged" | "now_playing";
+
+const playingStatus = computed(() => {
+  if (props.rom.rom_user.now_playing) return "now_playing";
+  if (props.rom.rom_user.backlogged) return "backlogged";
+  return props.rom.rom_user.status;
+});
+
+const statusToEmoji: Record<PlayingStatus, string> = {
+  backlogged: "🔜",
+  now_playing: "🕹️",
+  incomplete: "🚧",
+  finished: "🏁",
+  completed_100: "💯",
+  retired: "🏴",
+  never_playing: "🚫",
+};
+
+const statusToOption: Record<PlayingStatus, string> = {
+  backlogged: "Backlogged (will play soon)",
+  now_playing: "Now playing",
+  incomplete: "Incomplete (started but not finished)",
+  finished: "Finished (reached the end)",
+  completed_100: "Completed (all levels, achivements, endings, etc...)",
+  retired: "Retired (won't play again)",
+  never_playing: "Never playing (will never play)",
+};
 </script>
 
 <template>
@@ -37,6 +67,14 @@ const showSiblings = isNull(localStorage.getItem("settings.showSiblings"))
     <span class="emoji" v-for="language in rom.languages.slice(0, 3)">
       {{ languageToEmoji(language) }}
     </span>
+  </v-chip>
+  <v-chip
+    v-if="playingStatus"
+    class="translucent-dark mr-1 mt-1"
+    density="compact"
+    :title="statusToOption[playingStatus]"
+  >
+    {{ statusToEmoji[playingStatus] }}
   </v-chip>
   <v-chip
     v-if="rom.sibling_roms && rom.sibling_roms.length > 0 && showSiblings"
