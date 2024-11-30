6771956846325d573958ad995a887ce195e1a168
zurdi <zurdizurdo25@gmail.com>
2024-11-30 17:51:00 +0000

    fixed platform fields in response

--- frontend/src/__generated__/models/PlatformSchema.ts
+++ frontend/src/__generated__/models/PlatformSchema.ts
@@ -14,6 +14,8 @@ export type PlatformSchema = {
     igdb_id?: (number | null);
     sgdb_id?: (number | null);
     moby_id?: (number | null);
+    category?: (string | null);
+    generation?: (number | null);
     family_name?: (string | null);
     family_slug?: (string | null);
     url?: (string | null);
