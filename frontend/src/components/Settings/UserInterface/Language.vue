1af040b653fa98a99936c50568710d133df70fda
zurdi <zurdizurdo25@gmail.com>
2024-12-09 11:44:06 +0000

    feat: added user interface and library management to internationalization

--- frontend/src/components/Settings/UserInterface/Language.vue
+++ frontend/src/components/Settings/UserInterface/Language.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import RSection from "@/components/common/RSection.vue";
 import LanguageSelector from "@/components/Settings/UserInterface/LanguageSelector.vue";
+import { useI18n } from "vue-i18n";
+
+// Props
+const { t } = useI18n();
 </script>
 <template>
-  <r-section icon="mdi-translate" title="Language">
+  <r-section icon="mdi-translate" :title="t('settings.language')">
     <template #content>
       <language-selector />
     </template>
