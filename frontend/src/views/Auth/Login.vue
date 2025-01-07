04c31374afd49de014761e080ac96a95cc172a94
Georges-Antoine Assi <contact@me.gantoine.com>
2025-01-07 17:28:13 -0500

    disable login endpoint as wel

--- frontend/src/views/Auth/Login.vue
+++ frontend/src/views/Auth/Login.vue
@@ -74,10 +74,10 @@ async function loginOIDC() {
 
 <template>
   <v-card class="translucent-dark py-8 px-5" width="500">
-    <v-img src="/assets/isotipo.svg" class="mx-auto" width="150" />
+    <v-img src="/assets/isotipo.svg" class="mx-auto mb-4" width="150" />
     <v-row class="text-white justify-center mt-2" no-gutters>
       <v-col cols="10">
-        <v-form @submit.prevent="login">
+        <v-form v-if="!loginDisabled" @submit.prevent="login">
           <v-text-field
             v-model="username"
             :label="t('login.username')"
@@ -99,7 +99,6 @@ async function loginOIDC() {
             variant="underlined"
           />
           <v-btn
-            v-if="!loginDisabled"
             type="submit"
             class="bg-terciary mt-4"
             variant="text"
@@ -120,48 +119,48 @@ async function loginOIDC() {
               />
             </template>
           </v-btn>
-          <template v-if="oidcEnabled">
-            <v-divider v-if="!loginDisabled" class="my-4">
-              <template #default>
-                <span class="px-1">{{ t("login.or") }}</span>
-              </template>
-            </v-divider>
-            <v-btn
-              block
-              type="submit"
-              class="bg-terciary"
-              variant="text"
-              :disabled="loggingInOIDC || loggingIn"
-              :loading="loggingInOIDC"
-              @click="loginOIDC()"
-            >
-              <template v-if="oidcProvider" #prepend>
-                <v-icon size="20">
-                  <v-img
-                    :src="`/assets/dashboard-icons/${oidcProvider.toLowerCase()}.png`"
-                  >
-                    <template #error>
-                      <v-icon size="20">mdi-key</v-icon>
-                    </template>
-                  </v-img>
-                </v-icon>
-              </template>
-              {{
-                t("login.login-oidc", {
-                  oidc: oidcProvider || "OIDC",
-                })
-              }}
-              <template #loader>
-                <v-progress-circular
-                  color="romm-accent-1"
-                  :width="2"
-                  :size="20"
-                  indeterminate
-                />
-              </template>
-            </v-btn>
-          </template>
         </v-form>
+        <template v-if="oidcEnabled">
+          <v-divider v-if="!loginDisabled" class="my-4">
+            <template #default>
+              <span class="px-1">{{ t("login.or") }}</span>
+            </template>
+          </v-divider>
+          <v-btn
+            block
+            type="submit"
+            class="bg-terciary"
+            variant="text"
+            :disabled="loggingInOIDC || loggingIn"
+            :loading="loggingInOIDC"
+            @click="loginOIDC()"
+          >
+            <template v-if="oidcProvider" #prepend>
+              <v-icon size="20">
+                <v-img
+                  :src="`/assets/dashboard-icons/${oidcProvider.toLowerCase()}.png`"
+                >
+                  <template #error>
+                    <v-icon size="20">mdi-key</v-icon>
+                  </template>
+                </v-img>
+              </v-icon>
+            </template>
+            {{
+              t("login.login-oidc", {
+                oidc: oidcProvider || "OIDC",
+              })
+            }}
+            <template #loader>
+              <v-progress-circular
+                color="romm-accent-1"
+                :width="2"
+                :size="20"
+                indeterminate
+              />
+            </template>
+          </v-btn>
+        </template>
       </v-col>
     </v-row>
   </v-card>
