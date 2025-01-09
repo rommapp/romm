023b69ae85aa439b60c7c97fdb8ed2b243bd466a
Georges-Antoine Assi <contact@me.gantoine.com>
2025-01-08 22:24:24 -0500

    cleanup some svgs

--- frontend/src/components/common/RIsotipo.vue
+++ frontend/src/components/common/RIsotipo.vue
@@ -1,9 +1,19 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 withDefaults(defineProps<{ size?: number }>(), { size: 40 });
+
+const logos = [
+  "romm_logo_xbox_one_circle.svg",
+  "romm_logo_ps2_circle.svg",
+  "romm_logo_snes_circle.svg",
+];
+
+const randomLogo = ref<string>(logos[Math.floor(Math.random() * logos.length)]);
 </script>
 
 <template>
   <v-avatar :size="size">
-    <img src="/assets/isotipo.svg" :width="size" />
+    <img :src="`/assets/logos/${randomLogo}`" alt="Romm Logo" :width="size" />
   </v-avatar>
 </template>
