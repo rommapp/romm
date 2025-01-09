8e44e6f9e79ae70d47e85a5328918c6f7e6e07bb
Georges-Antoine Assi <contact@me.gantoine.com>
2025-01-09 12:21:45 -0500

    set randomized weights to logo

--- frontend/src/components/common/RIsotipo.vue
+++ frontend/src/components/common/RIsotipo.vue
@@ -3,13 +3,26 @@ import { ref } from "vue";
 
 withDefaults(defineProps<{ size?: number }>(), { size: 40 });
 
-const logos = [
-  "romm_logo_xbox_one_circle.svg",
-  "romm_logo_ps2_circle.svg",
-  "romm_logo_snes_circle.svg",
-];
+const logos = {
+  xbox: { path: "romm_logo_xbox_one_circle.svg", weight: 0.945 },
+  ps2: { path: "romm_logo_ps2_circle.svg", weight: 0.05 },
+  snes: { path: "romm_logo_snes_circle.svg", weight: 0.005 },
+};
 
-const randomLogo = ref<string>(logos[Math.floor(Math.random() * logos.length)]);
+const getRandomLogo = () => {
+  const random = Math.random();
+  let sum = 0;
+
+  for (const { path, weight } of Object.values(logos)) {
+    sum += weight;
+    if (random <= sum) return path;
+  }
+
+  return logos.xbox.path;
+};
+
+// Easter egg: Random logo on each render
+const randomLogo = ref<string>(getRandomLogo());
 </script>
 
 <template>
