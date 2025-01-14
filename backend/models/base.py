195b0cf742f6db20e89e75e7f219e1c4f9de46ce
Georges-Antoine Assi <contact@me.gantoine.com>
2025-01-13 21:50:18 -0500

    [HOTFIX] Change all DateTime columns to TIMESTAMP

--- backend/models/base.py
+++ backend/models/base.py
@@ -1,13 +1,13 @@
 from datetime import datetime
 
-from sqlalchemy import DateTime, func
+from sqlalchemy import TIMESTAMP, func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class BaseModel(DeclarativeBase):
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now()
+        TIMESTAMP(timezone=True), server_default=func.now()
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), onupdate=func.now()
+        TIMESTAMP(timezone=True), server_default=func.now(), onupdate=func.now()
     )
