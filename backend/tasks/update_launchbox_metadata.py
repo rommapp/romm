446102d387c66377fc94457eab36f0e1ffe324b7
Georges-Antoine Assi <contact@me.gantoine.com>
2025-01-18 14:20:43 -0500

    complete parsing and setting metadata

--- backend/tasks/update_launchbox_metadata.py
+++ backend/tasks/update_launchbox_metadata.py
@@ -20,6 +20,8 @@ from utils.context import initialize_context
 LAUNCHBOX_PLATFORMS_KEY: Final = "romm:launchbox_platforms"
 LAUNCHBOX_METADATA_DATABASE_ID_KEY: Final = "romm:launchbox_metadata_database_id"
 LAUNCHBOX_METADATA_NAME_KEY: Final = "romm:launchbox_metadata_name"
+LAUNCHBOX_METADATA_IMAGE_KEY: Final = "romm:launchbox_metadata_image"
+LAUNCHBOX_MAME_KEY: Final = "romm:launchbox_mame"
 
 
 class UpdateLaunchboxMetadataTask(RemoteFilePullTask):
@@ -39,9 +41,7 @@ class UpdateLaunchboxMetadataTask(RemoteFilePullTask):
             return
 
         try:
-            with zipfile.ZipFile(
-                "/Users/georges-antoine/workspace/romm/backend/tasks/Metadata.zip", "r"
-            ) as z:
+            with zipfile.ZipFile(content) as z:
                 for file in z.namelist():
                     if file == "Platforms.xml":
                         platform_dict = {}
@@ -54,54 +54,100 @@ class UpdateLaunchboxMetadataTask(RemoteFilePullTask):
                                 }
 
                             async with async_cache.pipeline() as pipe:
-                                for data_batch in batched(platform_dict.items(), 2000):
-                                    metadata_map = {
-                                        k: json.dumps(v)
-                                        for k, v in dict(data_batch).items()
-                                    }
+                                for platform_batch in batched(
+                                    platform_dict.items(), 2000
+                                ):
                                     await pipe.hset(
-                                        LAUNCHBOX_PLATFORMS_KEY, mapping=metadata_map
+                                        LAUNCHBOX_PLATFORMS_KEY,
+                                        mapping={
+                                            k: json.dumps(v)
+                                            for k, v in dict(platform_batch).items()
+                                        },
                                     )
+
                     elif file == "Metadata.xml":
-                        metadata_by_id_dict = {}
-                        metadata_by_name_dict = {}
+                        metadata_by_id_dict: dict[str, object] = {}
+                        metadata_by_name_dict: dict[str, object] = {}
+                        metadata_images_by_id_dict: dict[str, list[object]] = {}
 
                         with z.open(file, "r") as f:
-                            for metadata in ET.parse(f).getroot().findall("Game"):
+                            root = ET.parse(f).getroot()
+                            for metadata in root.findall("Game"):
                                 id_elem = metadata.find("DatabaseID")
                                 assert id_elem is not None
-                                metadata_by_id_dict[id_elem.text] = {
+                                metadata_by_id_dict[str(id_elem.text)] = {
                                     child.tag: child.text for child in metadata
                                 }
 
                                 name_elem = metadata.find("Name")
                                 assert name_elem is not None
-                                metadata_by_name_dict[name_elem.text] = {
+                                metadata_by_name_dict[str(name_elem.text)] = {
                                     child.tag: child.text for child in metadata
                                 }
 
+                            for image in root.findall("GameImage"):
+                                id_elem = image.find("DatabaseID")
+                                assert id_elem is not None
+
+                                if id_elem.text not in metadata_images_by_id_dict:
+                                    metadata_images_by_id_dict[str(id_elem.text)] = []
+
+                                metadata_images_by_id_dict[str(id_elem.text)].append(
+                                    {child.tag: child.text for child in image}
+                                )
+
                             async with async_cache.pipeline() as pipe:
-                                for data_batch in batched(
+                                for mbid_batch in batched(
                                     metadata_by_id_dict.items(), 2000
                                 ):
-                                    titledb_map = {
-                                        k: json.dumps(v)
-                                        for k, v in dict(data_batch).items()
-                                    }
                                     await pipe.hset(
                                         LAUNCHBOX_METADATA_DATABASE_ID_KEY,
-                                        mapping=titledb_map,
+                                        mapping={
+                                            k: json.dumps(v)
+                                            for k, v in dict(mbid_batch).items()
+                                        },
+                                    )
+
+                                for mbn_batch in batched(
+                                    metadata_by_name_dict.items(), 2000
+                                ):
+                                    await pipe.hset(
+                                        LAUNCHBOX_METADATA_NAME_KEY,
+                                        mapping={
+                                            k: json.dumps(v)
+                                            for k, v in dict(mbn_batch).items()
+                                        },
                                     )
 
                                 for data_batch in batched(
-                                    metadata_by_name_dict.items(), 2000
+                                    metadata_images_by_id_dict.items(), 2000
                                 ):
-                                    titledb_map = {
-                                        k: json.dumps(v)
-                                        for k, v in dict(data_batch).items()
-                                    }
+
                                     await pipe.hset(
-                                        LAUNCHBOX_METADATA_NAME_KEY, mapping=titledb_map
+                                        LAUNCHBOX_METADATA_IMAGE_KEY,
+                                        mapping={
+                                            k: json.dumps(v)
+                                            for k, v in dict(data_batch).items()
+                                        },
+                                    )
+                    elif file == "Mame.xml":
+                        mame_dict = {}
+                        with z.open(file, "r") as f:
+                            for mame in ET.parse(f).getroot().findall("MameFile"):
+                                filename_elem = mame.find("FileName")
+                                assert filename_elem is not None
+                                mame_dict[filename_elem.text] = {
+                                    child.tag: child.text for child in mame
+                                }
+
+                            async with async_cache.pipeline() as pipe:
+                                for mame_batch in batched(mame_dict.items(), 2000):
+                                    await pipe.hset(
+                                        LAUNCHBOX_MAME_KEY,
+                                        mapping={
+                                            k: json.dumps(v)
+                                            for k, v in dict(mame_batch).items()
+                                        },
                                     )
         except zipfile.BadZipFile:
             log.error("Bad zip file in launchbox metadata update")
