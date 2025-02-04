bc01fe51301512c726c8848ad47a32e73bd2740b
zurdi <zurdizurdo25@gmail.com>
2025-02-04 11:59:17 +0000

    Update docker-compose ports and refactor Vue components for improved structure and functionality

--- frontend/src/components/common/Game/Dialog/ShowQRCode.vue
+++ frontend/src/components/common/Game/Dialog/ShowQRCode.vue
@@ -11,9 +11,11 @@ import qrcode from "qrcode";
 const { lgAndUp } = useDisplay();
 const show = ref(false);
 const emitter = inject<Emitter<Events>>("emitter");
+const rom = ref<SimpleRom>(null);
 
 emitter?.on("showQRCodeDialog", async (romToView: SimpleRom) => {
   show.value = true;
+  rom.value = romToView;
 
   await nextTick();
 
@@ -43,11 +45,13 @@ function closeDialog() {
     v-model="show"
     icon="mdi-pencil-box"
     scroll-content
-    :width="lgAndUp ? 400 : 300"
+    :width="lgAndUp ? 400 : 400"
   >
     <template #content>
       <v-row no-gutters>
-        <v-col cols="12" class="text-center">
+        <v-col cols="12" class="text-center px-4">
+          <h3 class="mt-5">{{ rom.name }}</h3>
+          <h4 class="text-primary">{{ rom.fs_name }}</h4>
           <canvas id="qr-code"></canvas>
         </v-col>
       </v-row>
