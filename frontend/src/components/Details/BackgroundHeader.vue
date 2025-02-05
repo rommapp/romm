6051a7eb665277a3794187f7fa6db9c1f8d19de6
Georges-Antoine Assi <contact@me.gantoine.com>
2025-02-04 19:28:36 -0500

    replace all remaining covers

--- frontend/src/components/Details/BackgroundHeader.vue
+++ frontend/src/components/Details/BackgroundHeader.vue
@@ -1,12 +1,22 @@
 <script setup lang="ts">
 import storeRoms from "@/stores/roms";
+import { getMissingCoverImage, getUnmatchedCoverImage } from "@/utils/covers";
 import { storeToRefs } from "pinia";
-import { useTheme } from "vuetify";
+import { computed } from "vue";
 
 // Props
-const theme = useTheme();
 const romsStore = storeRoms();
 const { currentRom } = storeToRefs(romsStore);
+const missingCoverImage = computed(() =>
+  getMissingCoverImage(
+    currentRom.value?.name || currentRom.value?.fs_name || "",
+  ),
+);
+const unmatchedCoverImage = computed(() =>
+  getUnmatchedCoverImage(
+    currentRom.value?.name || currentRom.value?.fs_name || "",
+  ),
+);
 </script>
 
 <template>
@@ -21,16 +31,14 @@ const { currentRom } = storeToRefs(romsStore);
       id="background-image"
       :src="
         !currentRom.igdb_id && !currentRom.moby_id && !currentRom.has_cover
-          ? `/assets/default/cover/${theme.global.name.value}_unmatched.svg`
+          ? unmatchedCoverImage
           : `/assets/romm/resources/${currentRom.path_cover_l}?ts=${currentRom.updated_at}`
       "
       lazy
       cover
     >
       <template #error>
-        <v-img
-          :src="`/assets/default/cover/${theme.global.name.value}_missing_cover.svg`"
-        />
+        <v-img :src="missingCoverImage" />
       </template>
       <template #placeholder>
         <div class="d-flex align-center justify-center fill-height">
