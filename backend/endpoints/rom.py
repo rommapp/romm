2e59497e2df743a277ec2f0b0f7a8400dafa8b23
Georges-Antoine Assi <contact@me.gantoine.com>
2025-03-02 14:22:56 -0500

    soft m3u files on download

--- backend/endpoints/rom.py
+++ backend/endpoints/rom.py
@@ -233,8 +233,8 @@ async def head_rom_content(
 
     file_ids = request.query_params.get("file_ids") or ""
     file_ids = [int(f) for f in file_ids.split(",") if f]
-    files = db_rom_handler.get_rom_files(rom.id)
-    files = [f for f in files if f.id in file_ids or not file_ids]
+    files = [f for f in rom.files if f.id in file_ids or not file_ids]
+    files.sort(key=lambda x: x.file_name)
 
     # Serve the file directly in development mode for emulatorjs
     if DEV_MODE:
@@ -307,8 +307,8 @@ async def get_rom_content(
 
     file_ids = request.query_params.get("file_ids") or ""
     file_ids = [int(f) for f in file_ids.split(",") if f]
-    files = db_rom_handler.get_rom_files(rom.id)
-    files = [f for f in files if f.id in file_ids or not file_ids]
+    files = [f for f in rom.files if f.id in file_ids or not file_ids]
+    files.sort(key=lambda x: x.file_name)
 
     log.info(f"User {current_username} is downloading {rom.fs_name}")
 
