1d029f4d36325439e82f919cc5ffdfc82e1a8239
Georges-Antoine Assi <contact@me.gantoine.com>
2025-03-02 14:14:29 -0500

    Sort list fields in pydantic

--- frontend/src/components/common/Game/Card/Flags.vue
+++ frontend/src/components/common/Game/Card/Flags.vue
@@ -62,7 +62,7 @@ const playingStatus = computed(() => {
     {{ getEmojiForStatus(playingStatus) }}
   </v-chip>
   <v-chip
-    v-if="rom.sibling_roms && rom.sibling_roms.length > 0 && showSiblings"
+    v-if="rom.sibling_roms.length > 0 && showSiblings"
     class="translucent-dark mr-1 mt-1"
     density="compact"
   >
