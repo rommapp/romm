3c0a38c5c7ea86da749f99a10bf35e340e8ffaa2
Georges-Antoine Assi <contact@me.gantoine.com>
2025-03-03 16:42:36 -0500

    cleanup more code

--- frontend/src/plugins/transition/index.ts
+++ frontend/src/plugins/transition/index.ts
@@ -1,10 +1,3 @@
-/**
- * @license
- * MIT License
- * Copyright (c) 2023 Aaron Zhou
- * https://github.com/Clarkkkk/vue-view-transitions/blob/main/LICENSE.md
- */
-
 interface ViewTransition {
   captured: Promise<void>;
   updateCallbackDone: Promise<void>;
@@ -16,33 +9,36 @@ interface ViewTransition {
 export function startViewTransition(
   callback?: () => Promise<void>,
 ): ViewTransition {
-  const viewTransition = {} as ViewTransition;
-  if (document.startViewTransition) {
-    const capturedPromise = new Promise<void>((resolve) => {
-      const nativeViewTransition = document.startViewTransition(async () => {
-        resolve();
-        if (callback) {
-          await callback();
-        }
-      });
-      viewTransition.updateCallbackDone =
-        nativeViewTransition.updateCallbackDone;
-      viewTransition.ready = nativeViewTransition.ready;
-      viewTransition.finished = nativeViewTransition.finished;
-      viewTransition.skipTransition =
-        nativeViewTransition.skipTransition.bind(nativeViewTransition);
-    });
-    viewTransition.captured = capturedPromise;
-  } else {
-    viewTransition.captured = Promise.resolve();
-    const callbackPromise = callback
-      ? Promise.resolve(callback())
-      : Promise.resolve();
-    viewTransition.updateCallbackDone =
-      viewTransition.ready =
-      viewTransition.finished =
-        callbackPromise;
-    viewTransition.skipTransition = () => {};
+  const callbackPromise = callback
+    ? Promise.resolve(callback())
+    : Promise.resolve();
+
+  const viewTransition = {
+    captured: Promise.resolve(),
+    updateCallbackDone: callbackPromise,
+    ready: callbackPromise,
+    finished: callbackPromise,
+    skipTransition: () => {},
+  };
+
+  if (!document.startViewTransition) {
+    return viewTransition;
   }
+
+  const capturedPromise = new Promise<void>((resolve) => {
+    const nativeViewTransition = document.startViewTransition(async () => {
+      resolve();
+      if (callback) {
+        await callback();
+      }
+    });
+    viewTransition.updateCallbackDone = nativeViewTransition.updateCallbackDone;
+    viewTransition.ready = nativeViewTransition.ready;
+    viewTransition.finished = nativeViewTransition.finished;
+    viewTransition.skipTransition =
+      nativeViewTransition.skipTransition.bind(nativeViewTransition);
+  });
+  viewTransition.captured = capturedPromise;
+
   return viewTransition;
 }
