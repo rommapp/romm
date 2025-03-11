7833052fb95b08a0c76a253bd30b4f143172cff5
Georges-Antoine Assi <contact@me.gantoine.com>
2025-03-11 00:41:04 -0400

    fix manual matching

--- frontend/src/components/common/Game/Dialog/MatchRom.vue
+++ frontend/src/components/common/Game/Dialog/MatchRom.vue
@@ -62,10 +62,6 @@ emitter?.on("showMatchRomDialog", (romToSearch) => {
     romToSearch.igdb_id || romToSearch.moby_id || romToSearch.ss_id
       ? (romToSearch.name ?? "")
       : romToSearch.fs_name_no_tags;
-
-  if (searchTerm.value) {
-    searchRom();
-  }
 });
 const missingCoverImage = computed(() =>
   getMissingCoverImage(rom.value?.name || rom.value?.fs_name || ""),
@@ -207,7 +203,19 @@ async function updateRom(selectedRom: SearchRomSchema) {
   show.value = false;
   emitter?.emit("showLoadingDialog", { loading: true, scrim: true });
 
-  Object.assign(rom.value, selectedRom);
+  // Set the properties from the selected rom
+  rom.value = {
+    ...rom.value,
+    igdb_id: selectedRom.igdb_id,
+    moby_id: selectedRom.moby_id,
+    ss_id: selectedRom.ss_id,
+    name: selectedRom.name,
+    slug: selectedRom.slug,
+    summary: selectedRom.summary,
+    url_cover: selectedRom.url_cover,
+  };
+
+  // Replace the cover image with a higher resolution
   if (rom.value.url_cover) {
     rom.value.url_cover = rom.value.url_cover.replace("t_cover_big", "t_1080p");
   }
