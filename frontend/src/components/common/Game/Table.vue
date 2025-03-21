581cdf742ccb911fc14a5b5f3a5900daa7f4f737
Georges-Antoine Assi <contact@me.gantoine.com>
2025-03-21 00:07:27 -0400

    fix pagination speeds

--- frontend/src/components/common/Game/Table.vue
+++ frontend/src/components/common/Game/Table.vue
@@ -192,11 +192,11 @@ function updateSelectedRom(rom: SimpleRom) {
         </v-row>
         <template #append>
           <v-chip
-            v-if="item.sibling_roms.length > 0 && showSiblings"
+            v-if="item.siblings.length > 0 && showSiblings"
             class="translucent-dark ml-4"
             size="x-small"
           >
-            <span class="text-caption">+{{ item.sibling_roms.length }}</span>
+            <span class="text-caption">+{{ item.siblings.length }}</span>
           </v-chip>
         </template>
       </v-list-item>
