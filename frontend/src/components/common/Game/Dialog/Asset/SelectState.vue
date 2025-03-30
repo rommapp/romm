5d2a149e9ea06ecb974fa74b643ee05a04825aee
Georges-Antoine Assi <contact@me.gantoine.com>
2025-03-30 15:55:03 -0400

    style fixes for save/state cards

--- frontend/src/components/common/Game/Dialog/Asset/SelectState.vue
+++ frontend/src/components/common/Game/Dialog/Asset/SelectState.vue
@@ -76,6 +76,7 @@ function closeDialog() {
                 <v-img
                   cover
                   height="100%"
+                  min-height="75px"
                   :src="
                     state.screenshot?.download_path ??
                     getEmptyCoverImage(state.file_name)
