6b11c1a93d153e8e497038b6191f224dba9ed59d
zurdi <zurdizurdo25@gmail.com>
2025-04-10 15:18:14 +0000

    fix: standardize vertical spacing in Gallery views

--- frontend/src/views/Gallery/Platform.vue
+++ frontend/src/views/Gallery/Platform.vue
@@ -265,7 +265,7 @@ onBeforeUnmount(() => {
 
         <!-- Gallery list view -->
         <v-row class="mr-13" v-if="currentView == 2" no-gutters>
-          <v-col class="mt-4">
+          <v-col class="my-4">
             <game-table class="mx-2" />
           </v-col>
         </v-row>
