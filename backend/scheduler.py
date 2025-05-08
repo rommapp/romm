f22e613d8485c6e704b34b3c1e0d2b057673734e
zurdi <zurdizurdo25@gmail.com>
2025-05-08 08:59:56 +0000

    refactor: enable scheduled tasks in scheduler and init scripts

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -1,20 +1,23 @@
 import sentry_sdk
-from config import SENTRY_DSN
+from config import (
+    ENABLE_SCHEDULED_RESCAN,
+    ENABLE_SCHEDULED_UPDATE_SWITCH_TITLEDB,
+    SENTRY_DSN,
+)
 from logger.logger import log
 from tasks.scan_library import scan_library_task
 from tasks.tasks import tasks_scheduler
 from tasks.update_switch_titledb import update_switch_titledb_task
 from utils import get_version
 
-sentry_sdk.init(
-    dsn=SENTRY_DSN,
-    release="romm@" + get_version(),
-)
+sentry_sdk.init(dsn=SENTRY_DSN, release=f"romm@{get_version()}")
 
 if __name__ == "__main__":
     # Initialize the tasks
-    scan_library_task.init()
-    update_switch_titledb_task.init()
+    if ENABLE_SCHEDULED_RESCAN:
+        scan_library_task.init()
+    if ENABLE_SCHEDULED_UPDATE_SWITCH_TITLEDB:
+        update_switch_titledb_task.init()
 
     log.info("Starting scheduler")
 
