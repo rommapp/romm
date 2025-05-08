c01a90caf6fb7fff7fff119f9224928b1742af98
zurdi <zurdizurdo25@gmail.com>
2025-05-08 14:36:32 +0000

    refactor: update Docker image tag to include 'testing' for local builds

--- docker/build_local_image.sh
+++ docker/build_local_image.sh
@@ -3,4 +3,4 @@
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 branch_name=${branch_name##refs/heads/}
 branch_name=${branch_name//\//-} # Replace slashes with dashes
-docker build -t "rommapp/romm:local-${branch_name}" . --file ./docker/Dockerfile
+docker build -t "rommapp/romm-testing:local-${branch_name}" . --file ./docker/Dockerfile
