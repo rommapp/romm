4c953b643947e0a3f8d0068c4dee4f7abaf2a7cb
zurdi <zurdizurdo25@gmail.com>
2025-05-08 14:30:48 +0000

    refactor: use symbolic links with force option in Dockerfile and improve branch name formatting in build script

--- docker/build_local_image.sh
+++ docker/build_local_image.sh
@@ -2,4 +2,5 @@
 
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name//\//-} # Replace slashes with dashes
 docker build -t "rommapp/romm:local-${branch_name}" . --file ./docker/Dockerfile
