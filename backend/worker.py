a9ac01cd077e85b805a2c3e7e915a06bca41f756
zurdi <zurdizurdo25@gmail.com>
2025-05-09 15:33:45 +0000

    refactor: update logging configuration for Alembic and RQ worker to unify logger formatting and level

--- backend/worker.py
+++ backend/worker.py
@@ -3,20 +3,11 @@ import logging
 import sentry_sdk
 from config import SENTRY_DSN
 from handler.redis_handler import redis_client
-from logger.logger import log
+from logger.logger import log, unify_logger
 from rq import Queue, Worker
 from utils import get_version
 
-# logger formatting
-rq_logger = logging.getLogger("rq.worker")
-rq_logger.setLevel(log.level)
-
-if not rq_logger.hasHandlers():
-    for handler in log.handlers:
-        rq_logger.addHandler(handler)
-else:
-    for handler in rq_logger.handlers:
-        handler.setFormatter(log.handlers[0].formatter)
+unify_logger("rq.worker")
 
 listen = ("high", "default", "low")
 
