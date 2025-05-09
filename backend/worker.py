1b38607eab85d33bd593e96268edb9a40b6bcd84
zurdi <zurdizurdo25@gmail.com>
2025-05-09 15:23:32 +0000

    refactor: update Alembic logging configuration and add ASCII art banner to init script

--- backend/worker.py
+++ backend/worker.py
@@ -7,19 +7,14 @@ from logger.logger import log
 from rq import Queue, Worker
 from utils import get_version
 
-# Get the rq.worker logger
+# logger formatting
 rq_logger = logging.getLogger("rq.worker")
-
-# Set its level (optional; you can match your app's LOGLEVEL if you want)
 rq_logger.setLevel(log.level)
 
-# Apply the same formatter to rq.worker handlers
 if not rq_logger.hasHandlers():
-    # You can reuse the same handler as your app logger OR create a new one
     for handler in log.handlers:
         rq_logger.addHandler(handler)
 else:
-    # If rq.worker already has handlers, just update their formatter
     for handler in rq_logger.handlers:
         handler.setFormatter(log.handlers[0].formatter)
 
