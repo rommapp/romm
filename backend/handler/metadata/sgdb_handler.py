53cf1bdc72498d68cc52cbfe44eec37afea2c8e6
Georges-Antoine Assi <contact@me.gantoine.com>
2025-08-06 11:04:24 -0400

    implement real lev distnce

--- backend/handler/metadata/sgdb_handler.py
+++ backend/handler/metadata/sgdb_handler.py
@@ -11,10 +11,24 @@ from logger.logger import log
 from .base_hander import MetadataHandler
 
 
-# Levenshtein distance implementation using difflib
 def levenshtein_distance(s1: str, s2: str) -> int:
-    """Simple Levenshtein distance implementation using difflib"""
-    return int((1 - SequenceMatcher(None, s1, s2).ratio()) * max(len(s1), len(s2)))
+    if len(s1) < len(s2):
+        return levenshtein_distance(s2, s1)
+
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = list(range(len(s2) + 1))
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1
+            deletions = current_row[j] + 1
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+
+    return previous_row[-1]
 
 
 # Used to display the Mobygames API status in the frontend
