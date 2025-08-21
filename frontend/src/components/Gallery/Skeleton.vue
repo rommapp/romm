bcb1e0003e00ea2d31f50f73cd97e883fe370584
Georges-Antoine Assi <contact@me.gantoine.com>
2025-08-21 09:58:37 -0400

    Use skeleton loaders as loading states for games

--- frontend/src/components/Gallery/Skeleton.vue
+++ frontend/src/components/Gallery/Skeleton.vue
@@ -2,9 +2,31 @@
 import { views } from "@/utils";
 import { storeToRefs } from "pinia";
 import storeGalleryView from "@/stores/galleryView";
+import storePlatforms from "@/stores/platforms";
+import { computed } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    platformId?: number;
+    aspectRatio?: string | number;
+  }>(),
+  {
+    platformId: undefined,
+    aspectRatio: undefined,
+  },
+);
 
 const galleryViewStore = storeGalleryView();
+const platformsStore = storePlatforms();
 const { currentView } = storeToRefs(galleryViewStore);
+
+const computedAspectRatio = computed(() => {
+  const ratio =
+    props.aspectRatio ||
+    platformsStore.getAspectRatio(props.platformId || 0) ||
+    galleryViewStore.defaultAspectRatioCover;
+  return parseFloat(ratio.toString());
+});
 </script>
 
 <template>
@@ -20,7 +42,11 @@ const { currentView } = storeToRefs(galleryViewStore);
           :lg="views[currentView]['size-lg']"
           :xl="views[currentView]['size-xl']"
         >
-          <v-skeleton-loader type="card" />
+          <v-skeleton-loader
+            type="image, avatar, chip, chip"
+            class="card-skeleton"
+            :style="{ aspectRatio: computedAspectRatio }"
+          />
         </v-col>
       </v-row>
 
@@ -35,3 +61,52 @@ const { currentView } = storeToRefs(galleryViewStore);
     </v-col>
   </v-row>
 </template>
+
+<style>
+.card-skeleton .v-skeleton-loader__card-avatar {
+  height: 100%;
+}
+
+.card-skeleton .v-skeleton-loader__image {
+  height: 100%;
+  border-radius: 4px;
+}
+
+.card-skeleton .v-skeleton-loader__avatar {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  margin: 4px;
+  min-height: 32px;
+  min-width: 32px;
+  max-height: 32px;
+  max-width: 32px;
+
+  &:after {
+    animation: none;
+  }
+}
+
+.card-skeleton .v-skeleton-loader__chip {
+  position: absolute;
+  font-size: 0.875rem;
+  padding: 0 12px;
+  height: 24px;
+  margin: 4px;
+
+  &:after {
+    animation: none;
+  }
+}
+
+.card-skeleton .v-skeleton-loader__chip:nth-of-type(3) {
+  bottom: 0;
+  right: 4px;
+}
+
+.card-skeleton .v-skeleton-loader__chip:nth-of-type(4) {
+  top: 0;
+  left: 0;
+}
+</style>
