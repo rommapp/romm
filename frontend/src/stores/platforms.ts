d58d250eae1bcae3850b1d50a905dd0e2b6abf58
Georges-Antoine Assi <contact@me.gantoine.com>
2025-08-22 10:39:14 -0400

    fetch through stores

--- frontend/src/stores/platforms.ts
+++ frontend/src/stores/platforms.ts
@@ -1,4 +1,5 @@
 import type { PlatformSchema } from "@/__generated__";
+import platformApi from "@/services/api/platform";
 import { uniqBy } from "lodash";
 import { defineStore } from "pinia";
 
@@ -8,6 +9,7 @@ export default defineStore("platforms", {
   state: () => ({
     allPlatforms: [] as Platform[],
     filterText: "" as string,
+    fetchingPlatforms: false as boolean,
   }),
 
   getters: {
@@ -26,11 +28,28 @@ export default defineStore("platforms", {
         )
         .sort((a, b) => a.display_name.localeCompare(b.display_name)),
   },
+
   actions: {
     _reorder() {
       this.allPlatforms = uniqBy(this.allPlatforms, "id").sort((a, b) => {
         return a.name.localeCompare(b.name);
       });
+    },
+    fetchPlatforms() {
+      if (this.fetchingPlatforms) return;
+      this.fetchingPlatforms = true;
+
+      platformApi
+        .getPlatforms()
+        .then(({ data: platforms }) => {
+          this.allPlatforms = platforms;
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          this.fetchingPlatforms = false;
+        });
     },
     set(platforms: Platform[]) {
       this.allPlatforms = platforms;
