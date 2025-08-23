ddf313c9ac4d3ffdc79a0b4578c120e817ab5337
Georges-Antoine Assi <contact@me.gantoine.com>
2025-08-23 15:25:05 -0400

    remove unused import

--- backend/models/platform.py
+++ backend/models/platform.py
@@ -4,7 +4,7 @@ from functools import cached_property
 from typing import TYPE_CHECKING
 
 from models.base import BaseModel
-from models.rom import Rom, RomFile
+from models.rom import Rom
 from sqlalchemy import String, func, select
 from sqlalchemy.orm import Mapped, column_property, mapped_column, relationship
 
