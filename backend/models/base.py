0a3c190cc5af70fb7e53701f8b75281713606d4b
Georges-Antoine Assi <contact@me.gantoine.com>
2025-09-17 11:14:03 -0400

    use utc_now func

--- backend/models/base.py
+++ backend/models/base.py
@@ -8,12 +8,16 @@ FILE_PATH_MAX_LENGTH = 1000
 FILE_EXTENSION_MAX_LENGTH = 100
 
 
+def utc_now() -> datetime:
+    return datetime.now(timezone.utc)
+
+
 class BaseModel(DeclarativeBase):
     created_at: Mapped[datetime] = mapped_column(
-        TIMESTAMP(timezone=True), default=lambda: datetime.now(timezone.utc)
+        TIMESTAMP(timezone=True), default=utc_now
     )
     updated_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
-        default=lambda: datetime.now(timezone.utc),
-        onupdate=lambda: datetime.now(timezone.utc),
+        default=utc_now,
+        onupdate=utc_now,
     )
