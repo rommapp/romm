cb3ebfc9cbf14ed8d3ea1b8f8d4b42aec4eddb9c
Georges-Antoine Assi <contact@me.gantoine.com>
2025-09-23 10:23:57 -0400

    progressive scan steps for each game

--- frontend/src/__generated__/models/SimpleRomSchema.ts
+++ frontend/src/__generated__/models/SimpleRomSchema.ts
@@ -56,6 +56,7 @@ export type SimpleRomSchema = {
     has_manual: boolean;
     path_manual: (string | null);
     url_manual: (string | null);
+    is_identifying?: boolean;
     is_unidentified: boolean;
     is_identified: boolean;
     revision: (string | null);
