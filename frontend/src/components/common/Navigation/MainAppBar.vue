fc12a3669f3ca7f0294799c2e7c8bbc53efa60cc
Georges-Antoine Assi <contact@me.gantoine.com>
2025-10-19 15:52:34 -0400

    [ROMM-2491] Add shuffle button in navbar

--- frontend/src/components/common/Navigation/MainAppBar.vue
+++ frontend/src/components/common/Navigation/MainAppBar.vue
@@ -2,6 +2,7 @@
 import { useLocalStorage } from "@vueuse/core";
 import { storeToRefs } from "pinia";
 import { useDisplay } from "vuetify";
+import RandomBtn from "@/components/Gallery/AppBar/common/RandomBtn.vue";
 import UploadRomDialog from "@/components/common/Game/Dialog/UploadRom.vue";
 import CollectionsBtn from "@/components/common/Navigation/CollectionsBtn.vue";
 import CollectionsDrawer from "@/components/common/Navigation/CollectionsDrawer.vue";
@@ -107,6 +108,7 @@ function collapse() {
     <ConsoleModeBtn :with-tag="!mainBarCollapsed" rounded class="mt-2" block />
 
     <template #append>
+      <RandomBtn :with-tag="!mainBarCollapsed" rounded class="mt-2" block />
       <UploadBtn
         :with-tag="!mainBarCollapsed"
         rounded
