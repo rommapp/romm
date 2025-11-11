08738c9784c15fac155f0b732e38d6479232bba0
Georges-Antoine Assi <contact@me.gantoine.com>
2025-11-11 11:22:27 -0500

    run trunk check

--- backend/alembic/versions/0025_roms_hashes.py
+++ backend/alembic/versions/0025_roms_hashes.py
@@ -9,11 +9,6 @@ Create Date: 2024-08-11 21:50:53.301352
 import sqlalchemy as sa
 from alembic import op
 
-from config import IS_PYTEST_RUN, SCAN_TIMEOUT
-from endpoints.sockets.scan import scan_platforms
-from handler.redis_handler import high_prio_queue
-from handler.scan_handler import ScanType
-
 # revision identifiers, used by Alembic.
 revision = "0025_roms_hashes"
 down_revision = "0024_sibling_roms_db_view"
