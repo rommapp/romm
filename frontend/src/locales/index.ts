5605a6a144766fe9e5afd36fe273f1867b57fbcf
Pavel Slabý <pavel.slaby@gmail.com>
2025-11-20 11:11:35 +0100

    Add cs_CZ PluralRules

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -27,6 +27,14 @@ const i18n = createI18n({
   locale: "en_US",
   fallbackLocale: "en_US",
   messages: loadLocales(),
+  pluralRules: {
+    cs_CZ(choice: number) {
+      if (choice === 0) return 0;
+      if (choice === 1) return 1;
+      if (choice >= 2 && choice <= 4) return 2;
+      return 3;
+    },
+  },
 });
 
 export default i18n;
