e1c30eb0d7066bf11c4b2e89780da80585da8d17
Pavel Slabý <pavel.slaby@gmail.com>
2025-11-20 12:02:19 +0100

    Update index.ts

--- frontend/src/locales/index.ts
+++ frontend/src/locales/index.ts
@@ -31,7 +31,7 @@ const i18n = createI18n({
     cs_CZ(choice: number) {
       if (choice === 0) return 0;
       if (choice === 1) return 1;
-      return (choice >= 2 && choice <= 4) ? 2 : 3;
+      return choice >= 2 && choice <= 4 ? 2 : 3;
     },
   },
 });
