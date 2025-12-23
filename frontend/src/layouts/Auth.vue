f8949c07865cffa0ab3ee80e2d0cd102152b1b52
zurdi <contact@zurdi.dev>
2025-12-23 13:04:54 +0000

    fix: update label for repeat password field in Setup.vue to correct translation key

--- frontend/src/layouts/Auth.vue
+++ frontend/src/layouts/Auth.vue
@@ -8,28 +8,15 @@ const heartbeatStore = storeHeartbeat();
 
 <template>
   <Notification />
-  <v-container id="container" class="pb-0 fill-height justify-center">
-    <v-col class="d-flex align-center justify-center pa-0" cols="12">
-      <router-view />
-    </v-col>
-    <v-container width="1100" class="pa-0 justify-center">
-      <v-card class="transparent pa-0">
-        <v-card-text class="pa-0">
-          <v-row no-gutters>
-            <v-col
-              class="d-flex align-center justify-space-between text-white text-shadow"
-            >
-              <LanguageSelector density="compact" />
-              <v-spacer /><v-spacer />
-              <span class="text-white text-subtitle-1 text-shadow">
-                {{ heartbeatStore.value.SYSTEM.VERSION }}
-              </span>
-            </v-col>
-          </v-row>
-        </v-card-text>
-      </v-card>
-    </v-container>
+  <v-container id="container" class="fill-height justify-center">
+    <router-view />
   </v-container>
+  <div id="language-selector">
+    <LanguageSelector density="compact" />
+  </div>
+  <span id="version" class="text-white text-subtitle-1 text-shadow">
+    {{ heartbeatStore.value.SYSTEM.VERSION }}
+  </span>
 </template>
 
 <style scoped>
@@ -38,6 +25,16 @@ const heartbeatStore = storeHeartbeat();
   background-size: cover;
   background-position: center;
   max-width: 100vw;
-  align-items: unset !important;
+  /* align-items: unset !important; */
+}
+#version {
+  position: absolute !important;
+  right: 15px !important;
+  bottom: 10px !important;
+}
+#language-selector {
+  position: absolute !important;
+  left: 10px !important;
+  bottom: 10px !important;
 }
 </style>
