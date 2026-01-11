100aca8fe1e0076214a413f39fca1c42688f5d9b
SaraVieira <hey@iamsaravieira.com>
2026-01-11 15:15:46 +0000

    flex mahgic

--- frontend/src/components/Details/Walkthroughs/WalkthroughHeader.vue
+++ frontend/src/components/Details/Walkthroughs/WalkthroughHeader.vue
@@ -12,31 +12,34 @@ const { getProgressLabel } = useWalkthrough({});
 <template>
   <v-expansion-panel-title class="bg-toplayer">
     <div class="d-flex align-center">
-      <v-chip size="small" class="mr-2" color="primary">
+      <v-chip size="x-small" class="mr-2" color="primary">
         {{ walkthrough.source }}
       </v-chip>
       <div class="d-flex flex-column">
-        <div class="text-body-2 font-weight-medium">
-          {{ walkthrough.title || walkthrough.url }}
-        </div>
+        <span class="text-body-2 font-weight-medium">
+          {{ walkthrough.title?.split("by")[0] || walkthrough.url }}
+        </span>
         <div class="text-caption text-medium-emphasis">
           <span v-if="walkthrough.author">By {{ walkthrough.author }}</span>
           <span v-else>{{ walkthrough.url }}</span>
         </div>
       </div>
     </div>
-    <v-chip size="x-small" color="primary" variant="tonal" class="ml-2">
-      {{ getProgressLabel(walkthrough) }}
-    </v-chip>
+
     <template #actions>
-      <v-btn
-        icon="mdi-open-in-new"
-        variant="text"
-        size="small"
-        :href="walkthrough.url"
-        target="_blank"
-        class="mr-1"
-      />
+      <div class="d-flex align-center">
+        <v-chip size="x-small" color="primary" variant="tonal" class="ml-2">
+          {{ getProgressLabel(walkthrough) }}
+        </v-chip>
+        <v-btn
+          icon="mdi-open-in-new"
+          variant="text"
+          size="small"
+          :href="walkthrough.url"
+          target="_blank"
+          class="mr-1"
+        />
+      </div>
     </template>
   </v-expansion-panel-title>
 </template>
