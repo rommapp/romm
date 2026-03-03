3aa3b9668ee97ba617e849e79ba031e31d5b7331
Georges-Antoine Assi <contact@me.gantoine.com>
2026-03-03 16:53:39 -0500

    generate ts defs

--- frontend/src/__generated__/models/DetailedRomSchema.ts
+++ frontend/src/__generated__/models/DetailedRomSchema.ts
@@ -76,6 +76,7 @@ export type DetailedRomSchema = {
     crc_hash: (string | null);
     md5_hash: (string | null);
     sha1_hash: (string | null);
+    ra_hash: (string | null);
     has_simple_single_file: boolean;
     has_nested_single_file: boolean;
     has_multiple_files: boolean;
