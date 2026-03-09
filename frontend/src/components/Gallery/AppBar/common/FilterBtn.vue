3286fd970ecd683006399813296e43a92ecded9f
Georges-Antoine Assi <contact@me.gantoine.com>
2026-03-09 09:13:14 -0400

    ill do it myself

--- frontend/src/components/Gallery/AppBar/common/FilterBtn.vue
+++ frontend/src/components/Gallery/AppBar/common/FilterBtn.vue
@@ -2,10 +2,13 @@
 import { storeToRefs } from "pinia";
 import { useI18n } from "vue-i18n";
 import storeGalleryFilter from "@/stores/galleryFilter";
+import storeRoms from "@/stores/roms";
 
 const { t } = useI18n();
 const galleryFilterStore = storeGalleryFilter();
 const { activeFilterDrawer } = storeToRefs(galleryFilterStore);
+const romsStore = storeRoms();
+const { fetchTotalRoms } = storeToRefs(romsStore);
 </script>
 
 <template>
@@ -18,11 +21,10 @@ const { activeFilterDrawer } = storeToRefs(galleryFilterStore);
   >
     <template #activator="{ props }">
       <v-btn
-        class="ml-0"
+        class="ml-0 px-3 min-w-auto! h-full!"
         variant="text"
         rounded="0"
         v-bind="props"
-        icon="mdi-filter-variant"
         :color="
           activeFilterDrawer
             ? 'primary'
@@ -32,7 +34,12 @@ const { activeFilterDrawer } = storeToRefs(galleryFilterStore);
         "
         aria-label="Filter"
         @click="galleryFilterStore.switchActiveFilterDrawer"
-      />
+      >
+        <div class="d-flex flex-col align-center">
+          <span class="text-caption leading-none!">{{ fetchTotalRoms }}</span>
+          <v-icon size="x-large">mdi-filter-variant</v-icon>
+        </div>
+      </v-btn>
     </template>
   </v-tooltip>
 </template>
