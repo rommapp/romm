76f70661b232f10db9002e65deecead6b3150b46
Yukine <devyukine@gmx.de>
2026-09-03 18:04:08 +0200

    feat(converto): add conversion settings UI with per-platform format policies

---
+++ frontend/src/__generated__/models/ConverttoConfig.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type ConverttoConfig = {
+    download_conversion_enabled: boolean;
+    scan_metadata: boolean;
+    cache_ttl_hours: number;
+    platform_formats: Record<string, string>;
+};
+
